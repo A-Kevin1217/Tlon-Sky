99a08e475f4cfb687347fe6bd05e0cc0ab9f24f1
Tloml-Starry <11471174+Tloml-Starry@user.noreply.gitee.com>
2023-01-20 13:02:57 +0000

    添加指令#常驻先祖兑换图
    Signed-off-by: Tloml-Starry <11471174+Tloml-Starry@user.noreply.gitee.com>

--- apps/help.js
+++ apps/help.js
@@ -1,7 +1,5 @@
 import plugin from '../../../lib/plugins/plugin.js'
 import { segment } from "oicq";
-import fetch from 'node-fetch'
-import schedule from 'node-schedule'
 
 export class example extends plugin {
     constructor () {
@@ -12,29 +10,43 @@ export class example extends plugin {
         priority: 5000,
         rule: [
             {
-                reg: '^(SKY|Sky|sky|光遇)(帮助|菜单|使用说明)$',
-                fnc: 'skybz'
+              reg: '^(SKY|Sky|sky|光遇)(帮助|菜单|使用说明)$',
+              fnc: 'skybz'
             },
             {
-                reg: '^季节兑换图$',
-                fnc: 'jjdht'
+              reg: '^季节兑换图$',
+              fnc: 'jjdht'
             },
+            {
+              reg: '^常驻先祖兑换图$',
+              fnc: 'czxzdht'
+            }
         ]
     })
 }
   async skybz(e) {
-    const imgreply = 'https://gchat.qpic.cn/gchatpic_new/3620060826/2054658697-3188931212-F1315B129471B79DA55DEE10B8AC17D9/0?term=3&is_origin=0';
-    logger.info('[自助回复]', e.msg)
+    const Textreply = '\nSKY状态\n身高图\n季节兑换图\n常驻先祖兑换图';
+    logger.info('[SKY帮助]', e.msg)
     let msg = [
       segment.at(this.e.user_id),
-      imgreply ? segment.image(imgreply) : ""
+      Textreply ? Textreply : "",
     ]
     e.reply(msg)
     return true;
   }
   async jjdht(e) {
-    const imgreply = 'https://gchat.qpic.cn/gchatpic_new/3620060826/2054658697-2595398092-7E31C3691C641C877250CDDEBB2B89DE/0?term=3&is_origin=0'
+    const imgreply = 'plugins/SKY-GuangYu-plugin/resource/help/季节兑换图.png'
     logger.info('[季节兑换图]',e.msg)
+    let msg = [
+      segment.at(this.e.user_id),
+      imgreply ? segment.image(imgreply) : "",
+    ]
+    e.reply(msg)
+    return true;
+  }
+  async jjdht(e) {
+    const imgreply = 'plugins/SKY-GuangYu-plugin/resource/help/常驻兑换图.png'
+    logger.info('[常驻兑换图]',e.msg)
     let msg = [
       segment.at(this.e.user_id),
       imgreply ? segment.image(imgreply) : "",
