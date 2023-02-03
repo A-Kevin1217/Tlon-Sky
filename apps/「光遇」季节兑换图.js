a5fed935d2326f248a61e3cdd957f101c0902317
Tloml-Starry <zq3620060826@163.com>
2023-02-03 17:53:16 +0000

    修复
    Signed-off-by: Tloml-Starry <zq3620060826@163.com>

--- apps/「光遇」季节兑换图.js
+++ apps/「光遇」季节兑换图.js
@@ -192,7 +192,7 @@ export class example extends plugin {
     ])
   }
   async sky_cxbz(e) {
-    const imgreply = 'plugin/Tlon-Sky/resource/统计及其他/教程.png'
+    const imgreply = 'https://gchat.qpic.cn/gchatpic_new/3591203536/3882665563-2933672716-752D64627CEA1AE9F750F11C934579BD/0?term=3&is_origin=0'
     await this.reply([
       segment.at(this.e.user_id),
       imgreply ? segment.image(imgreply) : "",
