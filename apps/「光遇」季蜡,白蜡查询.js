61f809fa8c8968fde476966fdee9390257adf5e0
Tloml-Starry <zq3620060826@163.com>
2023-02-04 05:40:55 +0000

    update apps/「光遇」季蜡,白蜡查询.js.
    Signed-off-by: Tloml-Starry <zq3620060826@163.com>

--- apps/「光遇」季蜡,白蜡查询.js
+++ apps/「光遇」季蜡,白蜡查询.js
@@ -1,5 +1,13 @@
 import plugin from '../../../lib/plugins/plugin.js'
 import fetch from "node-fetch"
+import lodash from 'lodash'
+import fs from 'fs'
+import dayjs from 'dayjs'
+
+let datapath = `./data/Tlon-Sky_bl`
+fs.exists("./data/Tlon-Sky_bl",function (exists) {
+  if (!exists) fs.mkdirSync(".data/Tlon-Sky_bl")
+})
 let time1
 let change1
 let residual1
@@ -29,11 +37,11 @@ export class wenan extends plugin {
       let url = `http://plugin.skybay.cn:443/api/cx_w?id=${place}&cmd=bl`;
       let res = await fetch(url).catch((err) => logger.error(err))
       res = await res.json()
-      time1 = res.data[1].time
+      time1 = res.data[0].time
 
-      change1 = res.data[1].change
+      change1 = res.data[0].change
 
-      residual1 = res.data[1].residual
+      residual1 = res.data[0].residual
 
       await this.reply(`最近变化时间：${time1}\n变化数量：${change1}\n剩余蜡烛：${residual1}`, true)
     }
@@ -45,7 +53,7 @@ export class wenan extends plugin {
       res = await res.json()
       time1 = res.data[0].time
 
-      change1 = res.data[8].change
+      change1 = res.data[0].change
 
       residual1 = res.data[0].residual
 
