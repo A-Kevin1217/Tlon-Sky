04787ee57c93f8dd0c1baea00652c00ffda0f04b
Tloml-Starry <zq3620060826@163.com>
2023-02-04 17:20:43 +0000

    优化代码
    Signed-off-by: Tloml-Starry <zq3620060826@163.com>

--- apps/「光遇」季蜡,白蜡查询.js
+++ apps/「光遇」季蜡,白蜡查询.js
@@ -1,49 +1,43 @@
-import plugin from '../../../lib/plugins/plugin.js'
-import fetch from "node-fetch"
-
-let time1
-let change1
-let residual1
+import plugin from '../../../lib/plugins/plugin.js';
+import fetch from "node-fetch";
 
 export class wenan extends plugin {
-    constructor () {
-      super({
-        name: '光遇',
-        dsc: '蜡烛查询',
-        event: 'message',
-        priority: 5000,
-        rule: [
-          {
-            reg: `^(蜡烛查询)(.*)$`,
-            fnc: 'sky_lzcx'
-          },
-          {
-            reg: '^(季蜡查询)(.*)$',
-            fnc: 'sky_jlcx'
-          }
-        ]
-      })
-    }
-    async sky_lzcx(e) {
-      let msg = e.msg
-      let place = msg.replace(/#|蜡烛查询/g, "").trim();
-      let url = `http://plugin.skybay.cn:443/api/cx_w?id=${place}&cmd=bl`;
-      let res = await fetch(url).catch((err) => logger.error(err))
-      res = await res.json()
-      time1 = res.data[0].time
-      change1 = res.data[0].change
-      residual1 = res.data[0].residual
-      await this.reply(`最近变化时间：${time1}\n变化数量：${change1}\n剩余蜡烛：${residual1}`)
-    }
-    async sky_jlcx(e) {
-      let msg = e.msg
-      let place = msg.replace(/#|季蜡查询/g, "").trim();
-      let url = `http://plugin.skybay.cn:443/api/cx_w?id=${place}&cmd=jl`;
-      let res = await fetch(url).catch((err) => logger.error(err))
-      res = await res.json()
-      time1 = res.data[0].time
-      change1 = res.data[0].change
-      residual1 = res.data[0].residual
-      await this.reply(`最近变化时间：${time1}\n变化数量：${change1}\n剩余季蜡：${residual1}`)
-    }
+  constructor() {
+    super({
+      name: '光遇',
+      dsc: '蜡烛查询',
+      event: 'message',
+      priority: 5000,
+      rule: [
+        {
+          reg: `^(蜡烛查询)(.*)$`,
+          fnc: 'sky_lzcx'
+        },
+        {
+          reg: '^(季蜡查询)(.*)$',
+          fnc: 'sky_jlcx'
+        }
+      ]
+    });
   }
+
+  async sky_lzcx(e) {
+    let msg = e.msg;
+    let place = msg.replace(/#|蜡烛查询/g, "").trim();
+    let url = `http://plugin.skybay.cn:443/api/cx_w?id=${place}&cmd=bl`;
+    let res = await fetch(url).catch((err) => logger.error(err));
+    res = await res.json();
+    const { time, change, residual } = res.data[0];
+    await this.reply(`最近变化时间：${time}\n变化数量：${change}\n剩余蜡烛：${residual}`);
+  }
+  
+  async sky_jlcx(e) {
+    let msg = e.msg;
+    let place = msg.replace(/#|季蜡查询/g, "").trim();
+    let url = `http://plugin.skybay.cn:443/api/cx_w?id=${place}&cmd=jl`;
+    let res = await fetch(url).catch((err) => logger.error(err));
+    res = await res.json();
+    const { time, change, residual } = res.data[0];
+    await this.reply(`最近变化时间：${time}\n变化数量：${change}\n剩余季蜡：${residual}`);
+  }
+}
