0af97f5fca5d5eb8949a9f89aec98c17517da088
Tloml-Starry <zq3620060826@163.com>
2023-02-06 11:37:32 +0000

    光遇日历//更改名称以及描述并添加中文注释
    Signed-off-by: Tloml-Starry <zq3620060826@163.com>

--- apps/「光遇」光遇日历.js
+++ apps/「光遇」光遇日历.js
@@ -1,44 +1,55 @@
+// 引入插件基础类
 import plugin from '../../../lib/plugins/plugin.js'
+// 引入 oicq 模块中的segment
 import { segment } from "oicq";
+// 引入 puppeteer 模块
 import puppeteer from '../../../lib/puppeteer/puppeteer.js'
-
+// 定义 example 类，继承自 plugin 类
 export class example extends plugin {
+    // 构造函数
     constructor () {
       super({
-        name: '任务',
-        dsc: '光遇任务',
+      // 插件名称
+        name: '日历',
+      // 插件描述
+        dsc: '光遇日历',
+        // 事件类型       
         event: 'message',
+        // 优先级
         priority: 5000,
+        
+      // 规则数组
         rule: [
             {
+              // 正则表达式
               reg: '^(光遇)?日历$',
+              // 触发的函数名称
               fnc: 'rl'
             }
         ]
     })
 }
-  async rl(e) {
-		const browser = await puppeteer.browserInit();
-		const page = await browser.newPage();
-		await page.setViewport({
-			width: 650,
-			height: 2000
-		});
-		await page.goto('https://www.onebiji.com/hykb_tools/guangyu/rwgl/index.php');
-		let buff = null
-		
-		buff = await page.screenshot({
-			clip: {
-			  x: 31,
-			  y: 645,
-			  width: 591,
-			  height: 439,
-			}
-		});
-		
-		page.close().catch((err) => logger.error(err));
-		
-	await e.reply(segment.image(buff));
-	return true;
-  }
+// rl 函数，在规则匹配成功时被触发
+async rl(e) {
+  // 使用 puppeteer 库初始化浏览器
+	const browser = await puppeteer.browserInit();
+  // 打开一个新页面
+	const page = await browser.newPage();
+  // 设置页面大小
+	await page.setViewport({width: 650,height: 2000});
+  // 导航到特定 URL
+  await page.goto('https://www.onebiji.com/hykb_tools/guangyu/rwgl/index.php');
+  // 截取页面图像
+  const buff = await page.screenshot({
+    clip: {
+      x: 31, y: 645, width: 591, height: 439
+    }
+  });
+  // 关闭页面
+  await page.close();
+  // 回复图像数据
+  await e.reply(segment.image(buff));
+  // 返回 true
+  return true;
+}
 }
