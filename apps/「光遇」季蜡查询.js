21739cc924b5b5c71523fbdacff9835166a99e99
Tloml-Starry <zq3620060826@163.com>
2023-02-23 08:54:58 +0000

    ✅季蜡查询✅删改✅
    Signed-off-by: Tloml-Starry <zq3620060826@163.com>

--- apps/「光遇」季蜡查询.js
+++ apps/「光遇」季蜡查询.js
@@ -4,8 +4,8 @@ import fetch from "node-fetch";
 export class wenan extends plugin {
   constructor() {
     super({
-      name: '光遇',
-      dsc: '季蜡查询',
+      name: '光遇_季蜡查询',
+      dsc: '光遇',
       event: 'message',
       priority: 5000,
       rule: [
