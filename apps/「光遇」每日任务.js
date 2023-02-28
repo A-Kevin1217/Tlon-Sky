c75b3675fdb5b61d166ded62ae95c8b32452080c
Tloml-Starry <zq3620060826@163.com>
2023-02-28 05:11:45 +0000

    ✅.
    Signed-off-by: Tloml-Starry <zq3620060826@163.com>

--- apps/「光遇」每日任务.js
+++ apps/「光遇」每日任务.js
@@ -3,56 +3,56 @@
 //交流「宣群」q群:657862057 (大爷来玩呀)
 
 // 引入插件基础类
-import plugin from '../../../lib/plugins/plugin.js';
+//import plugin from '../../../lib/plugins/plugin.js';
 // 引入 oicq 模块中的segment
-import { segment } from "oicq";
+//import { segment } from "oicq";
 // 引入 puppeteer 模块
-import puppeteer from '../../../lib/puppeteer/puppeteer.js';
+//import puppeteer from '../../../lib/puppeteer/puppeteer.js';
 // 定义 example 类，继承自 plugin 类
-export class example extends plugin {
+//export class example extends plugin {
   // 构造函数
-  constructor() {
-    super({
+  //constructor() {
+    //super({
       // 插件名称
-      name: '光遇_今日任务',
+      //name: '光遇_今日任务',
       // 插件描述
-      dsc: '光遇',
+      //dsc: '光遇',
       // 事件类型                   
-      event: 'message',
+      //event: 'message',
       // 优先级
-      priority: 5000,
+      //priority: 5000,
       // 规则数组
-      rule: [
-        {
+      //rule: [
+        //{
           // 正则表达式
-          reg: '^#?(光遇)?(今日|每日)?任务$',
+          //reg: '^#?(光遇)?(今日|每日)?任务$',
           // 触发的函数名称
-          fnc: 'sky_JRRW'
-        }
-      ]
-    });
-  }
+          //fnc: 'sky_JRRW'
+        //}
+      //]
+    //});
+  //}
 // sky_JRRW 函数，在规则匹配成功时被触发
-async sky_JRRW(e) {
+//async sky_JRRW(e) {
   // 使用 puppeteer 库初始化浏览器
-  const browser = await puppeteer.browserInit();
+  //const browser = await puppeteer.browserInit();
   // 打开一个新页面
-  const page = await browser.newPage();
+  //const page = await browser.newPage();
   // 设置页面大小
-  await page.setViewport({ width: 1280, height: 1320 });
+  //await page.setViewport({ width: 1280, height: 1320 });
   // 导航到特定 URL
-  await page.goto('https://www.guoping123.com/hykb_tools/guangyu/rwgl/index.php');
+  //await page.goto('https://www.guoping123.com/hykb_tools/guangyu/rwgl/index.php');
   // 截取页面图像
-  const buff = await page.screenshot({
-    clip: {
-      x: 300, y: 1300, width: 675, height: 1800
-    }//x: 300, y: 1423, width: 675, height: 1800
-  });
+  //const buff = await page.screenshot({
+    //clip: {
+      //x: 300, y: 1300, width: 675, height: 1800
+    //}//x: 300, y: 1423, width: 675, height: 1800
+  //});
   // 关闭页面
-  await page.close();
+  //await page.close();
   // 回复图像数据
-  await e.reply(segment.image(buff));
+  //await e.reply(segment.image(buff));
   // 返回 true
-  return true;
-  }
-}
+  //return true;
+  //}
+//}
