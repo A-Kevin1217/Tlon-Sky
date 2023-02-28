150c908bcbaa8864e5389e7238be54610abbc5ef
Tloml-Starry <zq3620060826@163.com>
2023-02-28 05:12:30 +0000

    ✅光遇每日任务✅应天提供✅
    Signed-off-by: Tloml-Starry <zq3620060826@163.com>

---
+++ apps/「光遇」每日任务_应天提供.js
@@ -0,0 +1,26 @@
+import plugin from '../../../lib/plugins/plugin.js'
+import fetch from 'node-fetch'
+import{ segment }from 'oicq'
+
+export class wenan extends plugin {
+  constructor () {
+    super({
+      name: '光遇_每日任务',
+      dsc: '光遇',
+      event: 'message',
+      priority: 5000,
+      rule: [
+        {
+          reg: `^#?(光遇)?(今日|每日)?任务$`,
+          fnc: 'yt'
+        },
+      ]
+    })
+  }
+  async yt (e) {
+    let url = `https://api.t1qq.com/api/sky/gy/sc/scsky.php`;
+    let res = await fetch(url).catch((err) => logger.error(err))
+    await e.reply('正在返回图片,可能较慢', true)
+    await this.reply(segment.image(url), true)
+    }  
+}
