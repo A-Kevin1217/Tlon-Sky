9b24c10838d7233366b3b1b703cb25167549caec
Tloml-Starry <zq3620060826@163.com>
2023-03-04 09:00:16 +0000

    ✅季蜡位置_应天提供接口✅新增季蜡位置功能✅
    Signed-off-by: Tloml-Starry <zq3620060826@163.com>

---
+++ apps/「光遇」季蜡位置_应天提供接口.js
@@ -0,0 +1,26 @@
+import plugin from '../../../lib/plugins/plugin.js'
+import fetch from 'node-fetch'
+import{ segment }from 'oicq'
+
+export class wenan extends plugin {
+  constructor () {
+    super({
+      name: '光遇_季蜡位置',
+      dsc: '光遇',
+      event: 'message',
+      priority: 5000,
+      rule: [
+        {
+          reg: `^#?(光遇)?(季蜡)?位置$`,
+          fnc: 'yt'
+        },
+      ]
+    })
+  }
+  async yt (e) {
+    let url = `https://api.t1qq.com/api/sky/gy/sc/scjlwz.php`;
+    let res = await fetch(url).catch((err) => logger.error(err))
+    await e.reply('正在返回图片,可能较慢', true)
+    await this.reply(segment.image(url), true)
+    }  
+}
