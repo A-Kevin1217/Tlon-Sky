72bb2f9f9cc76377861c11a1377d2de51e7d55b6
Tloml-Starry <zq3620060826@163.com>
2023-03-06 17:19:02 +0000

    ✅季蜡查询✅小修改✅
    Signed-off-by: Tloml-Starry <zq3620060826@163.com>

--- apps/「光遇」季蜡查询.js
+++ apps/「光遇」季蜡查询.js
@@ -18,10 +18,10 @@ export class wenan extends plugin {
   }
 
   async sky_jlcx(e) {
+    await e.reply('正在查询中...\n返回速度较慢,请耐心等待\n数据更新取决于网易服务器\n如诺不对请等待3~5分钟再次查询', false, { recallMsg: 0 }, true)
     let msg = e.msg;
     let place = msg.replace(/#|季蜡查询/g, "").trim();
     let url = `https://api.t1qq.com/api/sky/gy/sc/scjl?id=${place}&type=jl`;
-    await e.reply('正在查询中...\n返回速度较慢,请耐心等待\n数据更新取决于网易服务器\n如诺不对请等待3~5分钟再次查询', false, { recallMsg: 0 }, true)
     await this.reply(segment.image(url), '如诺无数据,请发送‘上传角色+id’获取新id', false, { recallMsg: 0 }, true);
   }
 }
