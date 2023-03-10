ffa54a4ca4510bedc5a53708056aa4922e5eb1a1
Tloml-Starry <zq3620060826@163.com>
2023-03-10 20:35:08 +0000

    ✅新增功能✅[#绘画分析]✅可投稿,让图库增加一份!✅
    Signed-off-by: Tloml-Starry <zq3620060826@163.com>

---
+++ apps/「光遇」绘画分享.js
@@ -0,0 +1,30 @@
+import plugin from'../../../lib/plugins/plugin.js'
+import{segment}from'oicq'
+const path=process.cwd()
+let reply_img = 1.00
+let jpg_number = 30
+export class chuo extends plugin{
+    constructor() {
+        super({
+        name: '光遇_随机同人绘画',
+        dsc: '光遇',
+        event: 'message',
+        priority: 5000,
+        rule: [
+            {
+                reg: '#?绘画分享$',
+                fnc: 'sky_hhfx'
+            }
+        ]
+        })
+    }
+    async sky_hhfx (e){
+        let random_type = Math.random()
+        if(random_type < (reply_img)){
+            let photo_number = Math.ceil(Math.random() * (jpg_number))
+            if(photo_number<=jpg_number){
+                e.reply(segment.image('file:///' + path + '/plugins/Tlon-Sky/resource/光遇绘画分享/图像'+ photo_number + '.jpg'))
+            }
+        }
+    }
+}
