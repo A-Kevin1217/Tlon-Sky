e90aa5ed4878fa77a10478cec3efb307e53cf7e0
Tloml-Starry <zq3620060826@163.com>
2023-03-13 15:00:08 +0000

    ✅绘画分享✅38✅
    Signed-off-by: Tloml-Starry <zq3620060826@163.com>

--- apps/「光遇」绘画分享.js
+++ apps/「光遇」绘画分享.js
@@ -2,7 +2,7 @@ import plugin from'../../../lib/plugins/plugin.js'
 import{segment}from'oicq'
 const path=process.cwd()
 let reply_img = 1.00
-let jpg_number = 36
+let jpg_number = 38
 export class chuo extends plugin{
     constructor() {
         super({
