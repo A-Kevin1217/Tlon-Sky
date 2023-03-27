058dd950918f016da5fc69a91d8f510201f3662e
Tloml-Starry <zq3620060826@163.com>
2023-03-27 09:49:23 +0000

    ```将大蜡判断图和季蜡判断图合为一个JS```
    Signed-off-by: Tloml-Starry <zq3620060826@163.com>

--- apps/「光遇」判断图.js
+++ apps/「光遇」判断图.js
@@ -4,15 +4,19 @@ import moment from "moment";
 export class wenan extends plugin {
   constructor () {
     super({
-      name: '光遇_季蜡判断图',
+      name: '光遇_判断图',
       dsc: '光遇',
       event: 'message',
       priority: 5000,
       rule: [
         {
-          reg: `^#?(季蜡判断|任务)$`,
+          reg: /^#?季蜡判断$/,
           fnc: 'sky_jlpdt'
         },
+        {
+          reg: /^#?大蜡判断$/,
+          fnc: 'sky_dlpdt'
+        }
       ]
     })
   }
@@ -20,7 +24,7 @@ export class wenan extends plugin {
     let month = Number(moment().month()) + 1
     let monthKey = `Yz:count:sendMsg:month:`
     let messageCount = await redis.get(`${monthKey}${month}`) || 0
-    await e.reply('正在返回图片,可能较慢')
+    await e.reply('稍等...', true)
     let url = `https://api.t1qq.com/api/sky/gy/img/pdt/jl/yy.png`;
     let url2 = `https://api.t1qq.com/api/sky/gy/img/pdt/jl/yl.png`
     let url3 = `https://api.t1qq.com/api/sky/gy/img/pdt/jl/xg.png`
@@ -32,5 +36,18 @@ export class wenan extends plugin {
       '本月已发送',`${messageCount}`,'条消息'
     ]
     await this.reply(msg, true)
-    }  
+  }  
+  async sky_dlpdt (e) {
+    await e.reply('稍等...', true)
+    let url = `https://api.t1qq.com/api/sky/gy/img/pdt/dl/yy.png`;
+    let url2 = `https://api.t1qq.com/api/sky/gy/img/pdt/dl/yl.png`
+    let url3 = `https://api.t1qq.com/api/sky/gy/img/pdt/dl/xg.png`
+    let url4 = `https://api.t1qq.com/api/sky/gy/img/pdt/dl/mt.png`
+    let url5 = `https://api.t1qq.com/api/sky/gy/img/pdt/dl/jg.png`
+    let msg = [ 
+      '=====大蜡判断图======',segment.image(url),
+      segment.image(url2), segment.image(url3),segment.image(url4), segment.image(url5)
+    ]
+    await this.reply(msg, true)
+  } 
 }
