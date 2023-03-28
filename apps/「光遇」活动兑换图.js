ad45f7607d671bcb73a18b5e4cb5f5715a5a1e2a
Tloml-Starry <zq3620060826@163.com>
2023-03-28 13:04:32 +0000

    ```添加指令#花憩节兑换图```
    Signed-off-by: Tloml-Starry <zq3620060826@163.com>

--- apps/「光遇」活动兑换图.js
+++ apps/「光遇」活动兑换图.js
@@ -9,21 +9,36 @@ export class example extends plugin {
         priority: 5000,
         rule: [
             {
-              reg: '^#?(同心|情人)节兑换图$',
+              reg: /^#?(同心|情人)节兑换图$/,
               fnc: 'sky_txj'
             },
+            {
+              reg: /^#?花憩节兑换图$/,
+              fnc: 'sky_hqj'
+            }
         ]
     })
 }
   async sky_txj(e) {
-      const Textreply = '图源光萌攻略组\n如有侵权,联系仓库主人删除';
-      const imgreply = 'plugins/Tlon-Sky/resource/活动兑换图/同心节兑换图.png';
-      logger.info('[SKY]', e.msg)
-      let msg = [
-          imgreply ? segment.image(imgreply) : "",
-          Textreply ? Textreply : "",
-          ]
-          e.reply(msg, true)
-          return true;
-          }
-          }
+    const Textreply = '图源光萌攻略组\n如有侵权,联系仓库主人删除';
+    const imgreply = 'plugins/Tlon-Sky/resource/活动兑换图/同心节兑换图.png';
+    logger.info('[SKY]', e.msg)
+    let msg = [
+      imgreply ? segment.image(imgreply) : "",
+      Textreply ? Textreply : "",
+    ]
+    e.reply(msg, true)
+    return true;
+  }
+  async sky_hqj(e){
+    const Textreply = '图源光萌攻略组\n如有侵权,联系仓库主人删除';
+    const imgreply = 'plugins/Tlon-Sky/resource/活动兑换图/花憩节兑换图.png';
+    logger.info('[SKY]', e.msg)
+    let msg = [
+      imgreply ? segment.image(imgreply) : "",
+      Textreply ? Textreply : "",
+    ]
+    e.reply(msg, true)
+    return true;
+  }
+}
