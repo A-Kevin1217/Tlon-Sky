d3588b6758c92c6715d7e37acf64af803aed0d6a
Tloml-Starry <zq3620060826@163.com>
2023-04-04 16:17:23 +0000

    ```菜单文件```
    Signed-off-by: Tloml-Starry <zq3620060826@163.com>

---
+++ config/model/help_model.js
@@ -0,0 +1,103 @@
+//如需自定义清凉图帮助，请将改文件复制一份，粘贴到上一个目录（即此插件config文件夹内），并将粘贴过去的文件重命名为help.js，编辑完后重启云崽即可生效
+export const helpCfg = {
+  title: 'Sky帮助',
+  subTitle: 'Yunzai-Bot & Tlon-Sky',
+  columnCount: 0,
+  colWidth: 265,
+  theme: 'all',
+  themeExclude: ['default'],
+}
+export const helpList = [{
+  group: "基础功能",
+  list: [
+    {
+      icon: 2,
+      title: "光遇公告",
+      desc: "获取光遇官方公告"
+    },
+    {
+      icon: 5,
+      title: "光遇日历",
+      desc: "获取光遇最近活动,今日魔法等"
+    },
+    {
+      icon: 20,
+      title: "绘图分享",
+      desc: "欣赏光遇同人作品[图源国外友人]"
+    },
+    {
+      icon: 19,
+      title: "复刻预测",
+      desc: "随机挑选两位复刻先祖,仅供娱乐"
+    }, 
+    {
+      icon: 14,
+      title: "测量规则",
+      desc: "查看身高测量规则"
+    },
+    {
+      icon: 2,
+      title: "身高图",
+      desc: "获取光崽身高对比图片"
+    }
+  ]
+},{
+  group: '查询类功能',
+  list: [{
+    icon: 4,
+    title: '今日任务',
+    desc: '获取今日光遇任务'
+  },/*{
+    icon: 14,
+    title: '#开启/关闭体力大图',
+    desc: '体力形式：默认/卡片'
+  },{
+    icon: 48,
+    title: '#开启/关闭转大图',
+    desc: '配置是否开启转大图'
+  }, */{
+    icon: 1,
+    title: 'Sky状态',
+    desc: '获取当前服务器状态'
+  },{
+    icon: 18,
+    title: '查询教程',
+    desc: '获取白蜡&季蜡查询教程'
+  }, {
+    icon: 25,
+    title: '白蜡&季蜡查询+[id]',
+    desc: '查询蜡烛数量及最近变化时间'
+  }, {
+    icon: 100,
+    title: '',
+    desc: ''
+  }, {
+    icon: 24,
+    title: '季节剩余&活动剩余',
+    desc: '查询季节&活动剩余'
+  }]
+},{
+  group: '兑换图类功能',
+  list: [{
+    icon: 23,
+    title: '常驻兑换图',
+    desc: '获取常驻兑换图列表'
+  },{
+    icon: 22,
+    title: '活动兑换图',
+    desc: '获取活动兑换图列表'
+  },{
+    icon: 21,
+    title: '往期复刻图',
+    desc: '获取往期复刻图列表'
+  },{
+    icon: 100,
+    title: '',
+    desc: ''
+  },{
+    icon: 20,
+    title: '季节兑换图',
+    desc: '获取季节兑换图列表'
+  },]
+}
+]
