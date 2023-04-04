e95a99c1a562214fb7000d98c1cf6e99099644ad
Tloml-Starry <zq3620060826@163.com>
2023-04-04 16:35:40 +0000

    ```修改```
    Signed-off-by: Tloml-Starry <zq3620060826@163.com>

--- apps/「光遇」往期复刻.js
+++ apps/「光遇」往期复刻.js
@@ -1,6 +1,6 @@
 import plugin from '../../../lib/plugins/plugin.js'
 
-export class example extends plugin {
+export class 光遇_往期复刻 extends plugin {
     constructor () {
       super({
         name: '光遇_往期复刻',
