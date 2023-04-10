2918f6d336e03a570cd3111e1bd0fe3f755cfc18
Tloml-Starry <zq3620060826@163.com>
2023-04-10 17:57:24 +0000

    ```添加指令#Sky更新图库,可远程下载绘画图库，无需执行命令```
    Signed-off-by: Tloml-Starry <zq3620060826@163.com>

--- apps/「光遇」更新.js
+++ apps/「光遇」更新.js
@@ -1,9 +1,12 @@
 import plugin from '../../../lib/plugins/plugin.js'
-import lodash from 'lodash'
-import { createRequire } from 'module'
 import { Restart } from '../../other/restart.js'
+import { createRequire } from 'module'
+import lodash from 'lodash'
+import fs from 'fs'
 const require = createRequire(import.meta.url)
 const { exec, execSync } = require('child_process')
+const _path = process.cwd()
+const resPath = `${_path}/plugins/Tlon-Sky/resource/`;
 let uping = false
 
 export class 光遇_更新 extends plugin {
@@ -17,6 +20,10 @@ export class 光遇_更新 extends plugin {
                 {
                     reg: '^#?(光遇|sky|SKY|Sky)(插件)?(强制)?(更新)$',
                     fnc: 'sky_update'
+                },
+                {
+                    reg: '^#?(Sky更新图库|sky更新图库|光遇更新图库)$',
+                    fnc: 'sky_updateRes'
                 }
             ]
         })
@@ -29,56 +36,42 @@ export class 光遇_更新 extends plugin {
             return
         }
         let plugin = 'Tlon-Sky'
-
         if (!await this.checkGit()) return
-
         await this.runUpdate(plugin)
-
         if (this.isUp) {
             setTimeout(() => this.restart(), 2000)
         }
     }
-
     restart() {
         new Restart(this.e).restart()
     }
-
     async checkGit() {
         let ret = await execSync('git --version', { encoding: 'utf-8' })
         if (!ret || !ret.includes('git version')) {
             await this.reply('请先安装git')
             return false
         }
-
         return true
     }
-
     async runUpdate(plugin = '') {
         this.isNowUp = false
-
         let cm = `git -C ./plugins/${plugin}/ pull --no-rebase`
-
         let type = '更新'
         if (this.e.msg.includes('强制')) {
             type = '强制更新'
             cm = `git -C ./plugins/${plugin}/ checkout . && ${cm}`
         }
-
         this.oldCommitId = await this.getcommitId(plugin)
-
         await this.reply(`开始执行${type}操作...`)
         uping = true
         let ret = await this.execSync(cm)
         uping = false
-
         if (ret.error) {
             logger.mark(`${this.e.logFnc} 更新失败：${this.typeName}`)
             this.gitErr(ret.error, ret.stdout)
             return false
         }
-
         let time = await this.getTime(plugin)
-
         if (/Already up|已经是最新/g.test(ret.stdout)) {
             await this.reply(`${this.typeName}已经是最新\n最后更新时间：${time}`)
         } else {
@@ -87,60 +80,47 @@ export class 光遇_更新 extends plugin {
             let log = await this.getLog(plugin)
             await this.reply(log)
         }
-
         logger.mark(`${this.e.logFnc} 最后更新时间：${time}`)
-
         return true
     }
-
     async gitErr(err, stdout) {
         let msg = '更新失败！'
         let errMsg = err.toString()
         stdout = stdout.toString()
-
         if (errMsg.includes('Timed out')) {
             let remote = errMsg.match(/'(.+?)'/g)[0].replace(/'/g, '')
             await this.reply(msg + `\n连接超时：${remote}`)
             return
         }
-
         if (/Failed to connect|unable to access/g.test(errMsg)) {
             let remote = errMsg.match(/'(.+?)'/g)[0].replace(/'/g, '')
             await this.reply(msg + `\n连接失败：${remote}`)
             return
         }
-
         if (errMsg.includes('be overwritten by merge')) {
             await this.reply(msg + `存在冲突：\n${errMsg}\n` + '请解决冲突后再更新，或者执行#光遇强制更新，放弃本地修改')
             return
         }
-
         if (stdout.includes('CONFLICT')) {
             await this.reply([msg + '存在冲突\n', errMsg, stdout, '\n请解决冲突后再更新，或者执行#光遇强制更新，放弃本地修改'])
             return
         }
-
         await this.reply([errMsg, stdout])
     }
-
     async getcommitId(plugin = '') {
         let cm = 'git rev-parse --short HEAD'
         if (plugin) {
             cm = `git -C ./plugins/${plugin}/ rev-parse --short HEAD`
         }
-
         let commitId = await execSync(cm, { encoding: 'utf-8' })
         commitId = lodash.trim(commitId)
-
         return commitId
     }
-
     async getTime(plugin = '') {
         let cm = 'git log  -1 --oneline --pretty=format:"%cd" --date=format:"%m-%d %H:%M"'
         if (plugin) {
             cm = `cd ./plugins/${plugin}/ && git log -1 --oneline --pretty=format:"%cd" --date=format:"%m-%d %H:%M"`
         }
-
         let time = ''
         try {
             time = await execSync(cm, { encoding: 'utf-8' })
@@ -149,10 +129,8 @@ export class 光遇_更新 extends plugin {
             logger.error(error.toString())
             time = '获取时间失败'
         }
-
         return time
     }
-
     async execSync(cmd) {
         return new Promise((resolve, reject) => {
             exec(cmd, { windowsHide: true }, (error, stdout, stderr) => {
@@ -160,14 +138,11 @@ export class 光遇_更新 extends plugin {
             })
         })
     }
-
-
     async getLog(plugin = '') {
         let cm = 'git log  -20 --oneline --pretty=format:"%h||[%cd]  %s" --date=format:"%m-%d %H:%M"'
         if (plugin) {
             cm = `cd ./plugins/${plugin}/ && ${cm}`
         }
-
         let logAll
         try {
             logAll = await execSync(cm, { encoding: 'utf-8' })
@@ -175,11 +150,8 @@ export class 光遇_更新 extends plugin {
             logger.error(error.toString())
             this.reply(error.toString())
         }
-
         if (!logAll) return false
-
         logAll = logAll.split('\n')
-
         let log = []
         for (let str of logAll) {
             str = str.split('||')
@@ -189,14 +161,10 @@ export class 光遇_更新 extends plugin {
         }
         let line = log.length
         log = log.join('\n\n')
-
         if (log.length <= 0) return ''
-
         log = await this.makeForwardMsg(`${plugin}更新日志，共${line}条`, log)
-
         return log
     }
-
     async makeForwardMsg(title, msg) {
         let nickname = Bot.nickname
         if (this.e.isGroup) {
@@ -207,7 +175,6 @@ export class 光遇_更新 extends plugin {
             user_id: Bot.uin,
             nickname
         }
-
         let forwardMsg = [
             {
                 ...userInfo,
@@ -218,21 +185,61 @@ export class 光遇_更新 extends plugin {
                 message: msg
             }
         ]
-
         /** 制作转发内容 */
         if (this.e.isGroup) {
             forwardMsg = await this.e.group.makeForwardMsg(forwardMsg)
         } else {
             forwardMsg = await this.e.friend.makeForwardMsg(forwardMsg)
         }
-
         /** 处理描述 */
         forwardMsg.data = forwardMsg.data
             .replace(/\n/g, '')
             .replace(/<title color="#777777" size="26">(.+?)<\/title>/g, '___')
             .replace(/___+/, `<title color="#777777" size="26">${title}</title>`)
-
         return forwardMsg
     }
-
+    async sky_updateRes (e) {
+        if (!await checkAuth(e)) return
+        let command = '';
+        if (fs.existsSync(`${resPath}/光遇绘画分享/`)) {
+            // 图片包已经安装，尝试更新
+            e.reply('开始尝试更新，请耐心等待~');
+            command = 'git pull';
+            exec(command, { cwd: `${resPath}/光遇绘画分享/` }, function (error, stdout, stderr) {
+                console.log(stdout);
+                if (/(Already up[ -]to[ -]date|已经是最新的)/.test(stdout)) {
+                    e.reply('目前所有图片都已经是最新的~');
+                    return true;
+                }
+                let numRet = /(\d*) files changed,/.exec(stdout);
+                if (numRet && numRet[1]) {
+                    e.reply(`此次更新了${numRet[1]}张图片`);
+                    return true;
+                }
+                if (error) {
+                    e.reply('更新失败！\nError code: ' + error.code + '\n' + error.stack + '\n 请稍后重试。');
+                } else {
+                    e.reply('绘图分享图库更新成功~');
+                }
+            });
+        } else {
+            // 图片包未安装，尝试从Git仓库克隆
+            command = `git clone https://gitee.com/Tloml-Starry/Tlon-Sky-gallery.git "${resPath}/光遇绘画分享/" --depth=1`;
+            e.reply('开始尝试安装绘画分享图库\n可能会需要一段时间，请耐心等待~');
+            exec(command, function (error, stdout, stderr) {
+                if (error) {
+                    e.reply('绘画分享图库安装失败！\nError code: ' + error.code + '\n' + error.stack + '\n 请稍后重试。');
+                } else {
+                    e.reply('绘画分享图库安装成功\n您后续也可以通过[ #Sky更新图库 ]命令来更新图像');
+                }
+            });
+        }
+    }
 }
+const checkAuth = async function (e) {
+    if (!e.isMaster) {
+      e.reply(`只有主人才能命令哦~`)
+      return false
+    }
+    return true
+  }
