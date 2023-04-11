cda07045afd97e5e764a92a4c87854212ffab489
Tloml-Starry <zq3620060826@163.com>
2023-04-11 15:36:00 +0000

    ```更新帮助```
    Signed-off-by: Tloml-Starry <zq3620060826@163.com>

--- config/model/help_model.js
+++ config/model/help_model.js
@@ -9,66 +9,61 @@ export const helpCfg = {
 }
 export const helpList = [{
   group: "基础功能",
-  list: [
-    {
-      icon: 2,
-      title: "光遇公告",
-      desc: "获取光遇官方公告"
-    },
-    {
-      icon: 5,
-      title: "光遇日历",
-      desc: "获取光遇最近活动,今日魔法等"
-    },
-    {
-      icon: 20,
-      title: "绘图分享",
-      desc: "欣赏光遇同人作品[图源国外友人]"
-    },
-    {
-      icon: 19,
-      title: "复刻预测",
-      desc: "随机挑选两位复刻先祖,仅供娱乐"
-    }, 
-    {
-      icon: 14,
-      title: "测量规则",
-      desc: "查看身高测量规则"
-    },
-    {
-      icon: 2,
-      title: "身高图",
-      desc: "获取光崽身高对比图片"
-    }
-  ]
-},{
+  list: [{
+    icon: 2,
+    title: "光遇公告",
+    desc: "获取光遇官方公告"
+  },{
+    icon: 5,
+    title: "光遇日历",
+    desc: "获取光遇最近活动,今日魔法等"
+  },{
+    icon: 20,
+    title: "绘图分享",
+    desc: "欣赏光遇同人作品[图源国外友人]"
+  },{
+    icon: 19,
+    title: "复刻预测",
+    desc: "随机挑选两位复刻先祖,仅供娱乐"
+  },{
+    icon: 14,
+    title: "测量规则",
+    desc: "查看身高测量规则"
+  },{
+    icon: 2,
+    title: "身高图",
+    desc: "获取光崽身高对比图片"
+  }]
+},
+{
   group: '查询类功能',
   list: [{
     icon: 4,
     title: '今日任务',
     desc: '获取今日光遇任务'
-  }, {
+  },{
     icon: 1,
     title: 'Sky状态',
     desc: '获取当前服务器状态'
-  }, {
+  },{
     icon: 18,
     title: '查询教程',
     desc: '获取白蜡&季蜡查询教程'
-  }, {
+  },{
     icon: 25,
     title: '白蜡&季蜡查询+[id]',
     desc: '查询蜡烛数量及最近变化时间'
-  }, {
+  },{
     icon: 100,
-    title: '',
-    desc: ''
-  }, {
+    title: 'Sky版本',
+    desc: '查询当前版本更新动态'
+  },{
     icon: 24,
     title: '季节剩余&活动剩余',
     desc: '查询季节&活动剩余'
   }]
-},{
+},
+{
   group: '兑换图类功能',
   list: [{
     icon: 23,
@@ -90,6 +85,18 @@ export const helpList = [{
     icon: 20,
     title: '季节兑换图',
     desc: '获取季节兑换图列表'
-  },]
-}
-]
+  }]
+},
+{
+  group: '管理员功能',
+  auth: 'master',
+  list: [{
+    icon: 100,
+    title: 'Sky更新',
+    desc: '更新本插件'
+  },{
+    icon: 100,
+    title: 'Sky图库更新',
+    desc: '更新绘画分享图库'
+  }]
+}]
