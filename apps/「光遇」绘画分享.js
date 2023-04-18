a56b33f57e8ed1d4c664501aaa6f7c3a9fc1c76c
Tloml-Starry <zq3620060826@163.com>
2023-04-18 17:49:47 +0000

    ```咕咕```
    Signed-off-by: Tloml-Starry <zq3620060826@163.com>

--- apps/「光遇」绘画分享.js
+++ apps/「光遇」绘画分享.js
@@ -19,7 +19,7 @@ export class sky_hhfx extends plugin {
     async Sky_hhfx(e) {
         let Sky_image = `${process.cwd()}/plugins/Tlon-Sky/resource/光遇绘画分享/image/`;
         if (!fs.existsSync(Sky_image)) {
-          await e.reply("抱歉未找到光遇绘画分享文件夹！\n请使用指令 [#Sky更新图库安装]");
+          await e.reply("抱歉未找到光遇绘画分享文件夹！\n请使用指令 [#Sky更新图库] 安装");
           return false;
         }
         let img =
