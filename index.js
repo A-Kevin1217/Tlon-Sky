b8385eb4805debe830d667f050b3cc7e80287fad
Tloml-Starry <zq3620060826@163.com>
2023-04-29 16:39:12 +0000

    ```(撤回)```
    Signed-off-by: Tloml-Starry <zq3620060826@163.com>

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ if (!global.segment) {
 }
 
 const files = fs.readdirSync('./plugins/Tlon-Sky/apps').filter(file => file.endsWith('.js'))
-const files_2 = fs.readdirSync('./plugins/Tlon-Sky/apps/Tu').filter(file => file.endsWith('.js'))
 
 let ret = []
 
@@ -19,25 +18,12 @@ logger.info(` `)
 files.forEach((file) => {
   ret.push(import(`./apps/${file}`))
 })
-files_2.forEach((file) => {
-  ret.push(import(`./apps/Tu/${file}`))
-})
 
 ret = await Promise.allSettled(ret)
 
 let apps = {}
 for (let i in files) {
   let name = files[i].replace('.js', '')
-
-  if (ret[i].status != 'fulfilled') {
-    logger.error(`载入插件错误：${logger.red(name)}`)
-    logger.error(ret[i].reason)
-    continue
-  }
-  apps[name] = ret[i].value[Object.keys(ret[i].value)[0]]
-}
-for (let i in files_2) {
-  let name = files_2[i].replace('.js', '')
 
   if (ret[i].status != 'fulfilled') {
     logger.error(`载入插件错误：${logger.red(name)}`)
