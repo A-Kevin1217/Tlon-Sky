b9d126358db1fa9ff8928977791e98defbbe20ca
Tloml-Starry <zq3620060826@163.com>
2023-05-30 19:34:35 +0800

    ```上传角色使用二维码```

--- apps/「光遇」上传角色.js
+++ apps/「光遇」上传角色.js
@@ -18,6 +18,7 @@ export class 光遇_上传id extends plugin {
   }
 
   async sky_scid(e) {
-    await this.reply(`请扫码获取光遇id`,segment.image('https://gitee.com/Tloml-Starry/Tlon-picture/raw/master/%E4%B8%8A%E4%BC%A0%E8%A7%92%E8%89%B2%E4%BA%8C%E7%BB%B4%E7%A0%81.png'))
+    let url = 'https://gitee.com/Tloml-Starry/Tlon-picture/raw/master/上传角色二维码.png'
+    await this.reply(`请扫码获取光遇id`,segment.image(url))
     }
 }
