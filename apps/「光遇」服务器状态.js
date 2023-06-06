35f44ed6c4a3f6ca3aee6a4959d13fb0179f8062
Tloml-Starry <zq3620060826@163.com>
2023-06-06 12:00:50 +0800

    ```中文变量```

--- apps/「光遇」服务器状态.js
+++ apps/「光遇」服务器状态.js
@@ -21,15 +21,17 @@ export class wenan extends plugin {
   try {
     let response = await fetch(`https://live-queue-sky-merge.game.163.com/queue?type=json`);
     let res = await response.json();
+    let 排队人数 = res.pos
+    let 排队所需时间 = res.wait_time
     let num;
     if(res.ret === 0){
       num = `查询结果：未炸服~`;
     } else if(res.ret === 1){
-      num = `查询结果：炸服力~\n当前排队人数:「${res.pos}」\n预计所需时间:「${res.wait_time}」`;
+      num = `查询结果：炸服力~\n当前排队人数:「${排队人数}」\n预计所需时间:「${排队所需时间}」`;
     }
     msg = num;
   } catch (err) {
-    msg = '查询失败，光遇异常\n可能正在关服更新';
+    msg = '查询失败，光遇服务器异常\n可能正在关服更新';
   }
   e.reply(msg, true);
   }  
