5f2307cf795dc5d9fe79bd8f2528b07cee286033
Tloml-Starry <zq3620060826@163.com>
2023-06-09 13:15:43 +0800

    ```更新预言季集体复刻时间```

--- apps/「光遇」活动&季节剩余时间查询.js
+++ apps/「光遇」活动&季节剩余时间查询.js
@@ -45,29 +45,43 @@ async sky_jjsy(e) {
   return;
 }
 async sky_hdsy(e) {
+  let 活动指定时间1 = new Date('2023-6-16 23:59:59').getTime();
   let 活动指定时间 = new Date('2023-6-09 23:59:59').getTime();
   let msg = '';
+  let msg1 = '';
 
   function countdown() {
     let 现在时间 = Date.now();
     let 活动剩余时间_毫秒 = 活动指定时间 - 现在时间;
-
     if (活动剩余时间_毫秒 < 0) {
       msg = '海洋节已结束！';
       return;
     }
-
     let 活动剩余时间_天 = Math.floor(活动剩余时间_毫秒 / (24 * 60 * 60 * 1000));
     let 活动剩余时间_小时 = Math.floor((活动剩余时间_毫秒 % (24 * 60 * 60 * 1000)) / (60 * 60 * 1000));
     let 活动剩余时间_分钟 = Math.floor((活动剩余时间_毫秒 % (60 * 60 * 1000)) / (60 * 1000));
     let 活动剩余时间_秒 = Math.floor((活动剩余时间_毫秒 % (60 * 1000)) / 1000);
-
-
     msg = `距离海洋节结束还剩\n${活动剩余时间_天} 天 ${活动剩余时间_小时} 小时 ${活动剩余时间_分钟} 分钟 ${活动剩余时间_秒} 秒\n截止至2023-6-09 23:59:59`;
     setTimeout(countdown, 1000);
   }
+
+  function countdown() {
+    let 现在时间 = Date.now();
+    let 活动剩余时间_毫秒 = 活动指定时间1 - 现在时间;
+    if (活动剩余时间_毫秒 < 0) {
+      msg = '预言季集体复刻已结束！';
+      return;
+    }
+    let 活动剩余时间_天 = Math.floor(活动剩余时间_毫秒 / (24 * 60 * 60 * 1000));
+    let 活动剩余时间_小时 = Math.floor((活动剩余时间_毫秒 % (24 * 60 * 60 * 1000)) / (60 * 60 * 1000));
+    let 活动剩余时间_分钟 = Math.floor((活动剩余时间_毫秒 % (60 * 60 * 1000)) / (60 * 1000));
+    let 活动剩余时间_秒 = Math.floor((活动剩余时间_毫秒 % (60 * 1000)) / 1000);
+    msg1 = `距离预言季集体复刻结束还剩\n${活动剩余时间_天} 天 ${活动剩余时间_小时} 小时 ${活动剩余时间_分钟} 分钟 ${活动剩余时间_秒} 秒\n截止至2023-6-16 23:59:59`;
+    setTimeout(countdown, 1000);
+  }
+  reply = msg + msg1
   countdown();
-  await this.reply(msg, true);
+  await this.reply(reply, true);
   return;
 }
 }
