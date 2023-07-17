c678d0fe3151b6711defdd5e17caebe458e8b1a6
Tloml-Starry <zq3620060826@163.com>
2023-07-17 16:00:56 +0800

    ```修改文案```

--- apps/「光遇」服务器状态.js
+++ apps/「光遇」服务器状态.js
@@ -1,7 +1,8 @@
-import fetch from "node-fetch";
 import plugin from '../../../lib/plugins/plugin.js';
+import fetch from "node-fetch";
 
-export class wenan extends plugin {
+
+export class 光遇_服务器状态 extends plugin {
   constructor () {
     super({
       name: '光遇_服务器状态',
@@ -18,21 +19,21 @@ export class wenan extends plugin {
   }
   async Sky状态(e) { 
     let msg = '';
-  try {
-    let response = await fetch(`https://live-queue-sky-merge.game.163.com/queue?type=json`);
-    let res = await response.json();
-    let 排队人数 = res.pos
-    let 排队所需时间 = res.wait_time
-    let num;
-    if(res.ret === 0){
-      num = `查询结果：未炸服~`;
-    } else if(res.ret === 1){
-      num = `查询结果：炸服力~\n当前排队人数:「${排队人数}」\n预计所需时间:「${排队所需时间}」`;
+    try {
+      let response = await fetch(`https://live-queue-sky-merge.game.163.com/queue?type=json`);
+      let res = await response.json();
+      let 排队人数 = res.pos
+      let 排队所需时间 = res.wait_time
+      let num;
+      if(res.ret === 0){
+        num = `未炸服~`;
+      } else if(res.ret === 1){
+        num = `炸服力~\n当前排队人数:「${排队人数}」\n预计所需时间:「${排队所需时间}」`;
+      }
+      msg = num;
+    } catch (err) {
+      msg = '查询失败，光遇服务器异常';
     }
-    msg = num;
-  } catch (err) {
-    msg = '查询失败，光遇服务器异常';
+    e.reply(msg, true);
   }
-  e.reply(msg, true);
-  }  
 }
