96f7dc43bf1348439b7d0de6d81234b4db8da024
Tloml-Starry <zq3620060826@163.com>
2023-07-17 15:58:20 +0800

    ```移除注释```

--- apps/「光遇」绘画分享.js
+++ apps/「光遇」绘画分享.js
@@ -1,6 +1,7 @@
 import plugin from '../../../lib/plugins/plugin.js';
 import fs from "fs";
 
+
 export class sky_hhfx extends plugin {
   constructor() {
     super({
@@ -16,24 +17,15 @@ export class sky_hhfx extends plugin {
       ]
     })
   }
-
+  
   async 绘画分享(e) {
-    //定义一个变量Sky_image为项目中的指定路径
     let Sky_image = `${process.cwd()}/plugins/Tlon-Sky/resource/光遇绘画分享/image/`;
-    //如果这个路径下的文件夹不存在
     if (!fs.existsSync(Sky_image)) {
-      //发送错误消息
       await e.reply("抱歉未找到光遇绘画分享文件夹！\n请使用指令 [#Sky更新图库] 安装");
-      //返回false
       return false;
     }
-    //定义变量img为随机选择文件夹中的图片
     let 图片 = Sky_image + fs.readdirSync(Sky_image)[Math.floor(Math.random() * fs.readdirSync(Sky_image).length)];
-    //发送选中图片
-    await e.reply(
-      segment.image(`file:///${图片}`)
-      )
-      //返回true
+    await e.reply(segment.image(`file:///${图片}`))
     return true;
   }
 }
