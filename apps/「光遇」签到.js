64749a371b7d192ec60d765c3b4c9320b4f16574
Tloml-Starry <zq3620060826@163.com>
2023-07-19 21:12:33 +0800

    ```光遇签到添加累计次数```

--- apps/「光遇」签到.js
+++ apps/「光遇」签到.js
@@ -21,23 +21,26 @@ export class 光遇_签到 extends plugin {
     let msg = '';
     let userId = e.user_id;
     let userData = [];
+    let accumulatedDays = 0; // 累计签到天数
+    
     try {
       const fileData = fs.readFileSync(`plugins/Tlon-Sky/data/Sky签到/${userId}.json`, 'utf8');
       userData = JSON.parse(fileData);
+      accumulatedDays = userData.length; // 获取已有签到数据的长度作为累计签到天数
     } catch (err) {
       fs.writeFileSync(`plugins/Tlon-Sky/data/Sky签到/${userId}.json`, '[]', 'utf8');
     }
-  
+    
     const today = new Date().toISOString().slice(0, 10);
     const signInIndex = userData.findIndex(data => data.date === today);
-  
+    
     if (signInIndex !== -1) {
       msg = `您今天已经签到过了`;
-      e.reply(msg,true);
+      e.reply(msg, true);
     } else {
       let candles = Math.floor(Math.random() * 9) + 15;
       let quantity = candles;
-      console.error('签到获得：',candles);
+      console.error('签到获得：', candles);
       const yesterday = new Date(Date.now() - 24 * 60 * 60 * 1000).toISOString().slice(0, 10);
       const yesterdayIndex = userData.findIndex(data => data.date === yesterday);
       let continuousDays = 1;
@@ -52,9 +55,13 @@ export class 光遇_签到 extends plugin {
       };
       userData.push(signInData);
       fs.writeFileSync(`plugins/Tlon-Sky/data/Sky签到/${userId}.json`, JSON.stringify(userData), 'utf8');
-      msg = `签到成功，获得${quantity}根蜡烛\n当前共有${candles}根蜡烛\n连续签到${continuousDays}天`;
+    
+      accumulatedDays++; // 更新累计签到天数
+    
+      msg = `签到成功，获得${quantity}根蜡烛\n当前共有${candles}根蜡烛\n连续签到${continuousDays}天\n累计签到${accumulatedDays}天`;
       e.reply(msg, true);
     }
+    
   }
 }
 
