3fb64c14cbaf134b1872275dfc8e13f2920243d2
Tloml-Starry <zq3620060826@163.com>
2023-08-02 16:02:37 +0800

    ```改为html渲染图片发送```

--- apps/「光遇」我的信息.js
+++ apps/「光遇」我的信息.js
@@ -33,20 +33,46 @@ export class 我的信息 extends plugin {
             const 季蜡数 = userJson[userId]['季蜡']
             const 能量值 = userJson[userId]['能量值']
             const 等级 = userJson[userId]['等级']
-            const 信息 = [
-                `昵称：${昵称}\n`,
-                `等级：${等级}\n`,
-                `能量值：${能量值}/100\n`,
-                `白蜡数：${白蜡数}\n`,
-                `季蜡数：${季蜡数}\n`,
-                `连续签到：${连续签到天数}天\n`,
-                `累计签到：${累计签到天数}天\n`,
-                `最后签到日期：${最后签到日期}`
-            ]
-            e.reply(信息);
+
+            const html昵称 = `昵称：${昵称}`
+            const html等级 = `等级：${等级}`
+            const html能量值 = `能量值：${能量值}/100`
+            const html白蜡数 = `白蜡数：${白蜡数}`
+            const html季蜡数 = `季蜡数：${季蜡数}`
+            const html连续签到 = `连续签到：${连续签到天数}天`
+            const html累计签到 = `累计签到：${累计签到天数}天`
+            const html最后签到日期 = `最后签到日期：${最后签到日期}`
+            let html = {
+                昵称: html昵称,
+                头像: `https://api.t1qq.com/api/tool/qq/qqtx?key=lHV6bOsaNrsNv2hmegRRVMxOUp&qq=${userId}`,
+                等级: html等级,
+                能量值: html能量值,
+                白蜡数: html白蜡数,
+                季蜡数: html季蜡数,
+                连续签到: html连续签到,
+                累计签到: html累计签到,
+                最后签到日期: html最后签到日期
+              }
+              await render('admin/光遇信息', {
+                ...html,
+                bg: await rodom()
+              }, {
+                e,
+                scale: 1.4
+              })
         } catch (err) {
             logger.mark(err);
             e.reply('无法找到用户数据\n请先发送"光遇签到"');
         }
     }
 }
+
+const rodom = async function () {
+    let image = fs.readdirSync('./plugins/Tlon-Sky/resource/admin/imgs/bg')
+    let listImg = []
+    for (let val of image) {
+      listImg.push(val)
+    }
+    let imgs = listImg.length == 1 ? listImg[0] : listImg[lodash.random(0, listImg.length - 1)]
+    return imgs
+  }
