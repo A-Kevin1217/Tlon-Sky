2c512e617e57f1c99cbeae06752915a10ca19c3c
Tloml-Starry <zq3620060826@163.com>
2023-08-03 19:43:31 +0800

    ```移除```

--- apps/「光遇」绑定.js
+++ apps/「光遇」绑定.js
@@ -3,7 +3,6 @@ import fs from 'fs'
 
 const dirpath = "plugins/Tlon-Sky/data/id"
 let filename = `Sky ID.json`
-let 身高ID = 'Sky UID.json'
 if (!fs.existsSync(dirpath)) {
   fs.mkdirSync(dirpath);
 }
@@ -22,9 +21,6 @@ export class 光遇_绑定 extends plugin {
         {
           reg: /^#?绑定光遇id(.*)$/,
           fnc: '绑定光遇id'
-        },{
-          reg: /^#?绑定身高id(.*)$/,
-          fnc: '绑定身高id'
         }
       ]
     });
@@ -48,19 +44,5 @@ export class 光遇_绑定 extends plugin {
       fs.writeFileSync(dirpath + "/" + filename, JSON.stringify(json, null, "\t"));
       await this.reply("重新绑定成功")
     }
-  }
-
-  async 绑定身高id(e) {
-    const msg = e.msg;
-    const Sky_Uid = msg.replace(/#|绑定身高id/g, "").trim();
-    const data = {
-      Sky_Uid
-    }
-    const qq = e.user_id;
-    const json = JSON.parse(fs.readFileSync(dirpath + "/" + 身高ID, "utf8"));
-    json[qq] = data;
-    fs.writeFileSync(dirpath + "/" + 身高ID, JSON.stringify(json, null, "\t"));
-    const 消息 = json.hasOwnProperty(qq) ? "重新绑定成功" : "绑定成功\n您可使用'#身高查询'查询身高";
-    await this.reply(消息);
   }
 }
