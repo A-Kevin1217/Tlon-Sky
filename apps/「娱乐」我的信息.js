10241374964c000adcffa098f2caa9f440196b46
Tloml-Starry <zq3620060826@163.com>
2023-10-20 22:49:16 +0800

    * 2023/10/20

--- apps/「娱乐」我的信息.js
+++ apps/「娱乐」我的信息.js
@@ -14,85 +14,67 @@ export class 我的信息 extends plugin {
                 {
                     reg: '^#?光遇信息$',
                     fnc: '光遇信息'
+                },
+                {
+                    reg: '^#?光遇背包$',
+                    fnc: '光遇背包'
                 }
             ]
         });
     }
 
     async 光遇信息(e) {
-        const userId = e.user_id;
-        const filePath = `plugins/Tlon-Sky/data/Sky签到/${userId}.json`;
+        const 用户ID = e.user_id;
+        const 用户文件 = `plugins/Tlon-Sky/data/Sky签到/${用户ID}.json`;
 
         try {
-            const userData = await fs.promises.readFile(filePath);
-            const userJson = JSON.parse(userData.toString());
+            const 用户文件Data = await fs.promises.readFile(用户文件);
+            const 用户文件Json = JSON.parse(用户文件Data.toString());
 
-            const 昵称 = userJson[userId]['昵称']
-            const 最后签到日期 = userJson[userId]['最后签到日期']
-            const 连续签到天数 = userJson[userId]['连续签到天数']
-            const 累计签到天数 = userJson[userId]['累计签到天数']
-            const 白蜡数 = userJson[userId]['白蜡']
-            const 季蜡数 = userJson[userId]['季蜡']
-            const 能量值 = userJson[userId]['能量值']
-            const 等级 = userJson[userId]['等级']
-            const 抢数 = userJson[userId]['抢蜡烛次数'] || 0
-            const 被抢数 = userJson[userId]['被抢次数'] || 0
-            const 抢蜡烛总数 = userJson[userId]['抢蜡烛总数'] || 0
-            const 被抢蜡烛总数 = userJson[userId]['被抢蜡烛总数'] || 0
+            const 昵称 = 用户文件Json[用户ID]['昵称']
+            const 最后签到日期 = 用户文件Json[用户ID]['最后签到日期']
+            const 连续签到天数 = 用户文件Json[用户ID]['连续签到天数']
+            const 累计签到天数 = 用户文件Json[用户ID]['累计签到天数']
+            const 白蜡数 = 用户文件Json[用户ID]['白蜡']
+            const 季蜡数 = 用户文件Json[用户ID]['季蜡']
+            const 能量值 = 用户文件Json[用户ID]['能量值']
+            const 等级 = 用户文件Json[用户ID]['等级']
+            const 抢数 = 用户文件Json[用户ID]['抢蜡烛次数'] || 0
+            const 被抢数 = 用户文件Json[用户ID]['被抢次数'] || 0
+            const 抢蜡烛总数 = 用户文件Json[用户ID]['抢蜡烛总数'] || 0
+            const 被抢蜡烛总数 = 用户文件Json[用户ID]['被抢蜡烛总数'] || 0
             let 平均抢 = isNaN(抢蜡烛总数 / 抢数) ? 0 : (抢蜡烛总数 / 抢数).toFixed(1);
             let 平均被抢 = isNaN(被抢蜡烛总数 / 被抢数) ? 0 : (被抢蜡烛总数 / 被抢数).toFixed(1);
-            const 胜 = userJson[userId]['胜'] || 0
-            const 负 = userJson[userId]['负'] || 0
-            const 平 = userJson[userId]['平'] || 0
-            const 总赚取 = userJson[userId]['赚取'] || 0
-            const 总亏损 = userJson[userId]['亏损'] || 0
-            const 总赠送数量 = userJson[userId]['总赠送数量'] || 0
-            const 总收入数量 = userJson[userId]['总收入数量'] || 0
-
-            const html昵称 = `昵称：${昵称}`
-            const html等级 = `等级：${等级}`
-            const html能量值 = `能量值：${能量值}/100`
-            const html白蜡数 = `白蜡数：${白蜡数}`
-            const html季蜡数 = `季蜡数：${季蜡数}`
-            const html抢数 = `抢蜡烛次数：${抢数}`
-            const html被抢数 = `被抢次数：${被抢数}`
-            const html抢蜡烛总数 = `抢蜡烛总数：${抢蜡烛总数}根`
-            const html被抢蜡烛总数 = `被抢蜡烛总数：${被抢蜡烛总数}根`
-            const html连续签到 = `连续签到：${连续签到天数}天`
-            const html累计签到 = `累计签到：${累计签到天数}天`
-            const html最后签到日期 = `最后签到日期：${最后签到日期}`
-            const html胜 = `胜利次数：${胜}次`
-            const html负 = `失败次数：${负}次`
-            const html平 = `平局次数：${平}次`
-            const html总赚取 = `总获得：${总赚取}根`
-            const html总亏损 = `总亏损：${总亏损}根`
-            const html平均抢 = `平均抢得：${平均抢}根`
-            const html平均被抢 = `平均被抢：${平均被抢}根`
-            const html总赠送数量 = `总赠送：${总赠送数量}根`
-            const html总收入数量 = `总收入：${总收入数量}根`
+            const 胜 = 用户文件Json[用户ID]['胜'] || 0
+            const 负 = 用户文件Json[用户ID]['负'] || 0
+            const 平 = 用户文件Json[用户ID]['平'] || 0
+            const 总赚取 = 用户文件Json[用户ID]['赚取'] || 0
+            const 总亏损 = 用户文件Json[用户ID]['亏损'] || 0
+            const 总赠送数量 = 用户文件Json[用户ID]['总赠送数量'] || 0
+            const 总收入数量 = 用户文件Json[用户ID]['总收入数量'] || 0
             let html = {
-                昵称: html昵称,
-                头像: `https://api.t1qq.com/api/tool/qq/qqtx?key=lHV6bOsaNrsNv2hmegRRVMxOUp&qq=${userId}`,
-                等级: html等级,
-                能量值: html能量值,
-                白蜡数: html白蜡数,
-                季蜡数: html季蜡数,
-                抢次数: html抢数,
-                被抢次数: html被抢数,
-                抢蜡烛总数: html抢蜡烛总数,
-                被抢蜡烛总数: html被抢蜡烛总数,
-                连续签到: html连续签到,
-                累计签到: html累计签到,
-                最后签到日期: html最后签到日期,
-                胜: html胜,
-                负: html负,
-                平: html平,
-                总赚取: html总赚取,
-                总亏损: html总亏损,
-                平均抢: html平均抢,
-                平均被抢: html平均被抢,
-                总赠送数量: html总赠送数量,
-                总收入数量: html总收入数量
+                昵称: 昵称,
+                头像: `https://api.t1qq.com/api/tool/qq/qqtx?key=lHV6bOsaNrsNv2hmegRRVMxOUp&qq=${用户ID}`,
+                等级: 等级,
+                能量值: 能量值,
+                白蜡数: 白蜡数,
+                季蜡数: 季蜡数,
+                抢次数: 抢数,
+                被抢次数: 被抢数,
+                抢蜡烛总数: 抢蜡烛总数,
+                被抢蜡烛总数: 被抢蜡烛总数,
+                连续签到: 连续签到天数,
+                累计签到: 累计签到天数,
+                最后签到日期: 最后签到日期,
+                胜: 胜,
+                负: 负,
+                平: 平,
+                总赚取: 总赚取,
+                总亏损: 总亏损,
+                平均抢: 平均抢,
+                平均被抢: 平均被抢,
+                总赠送数量: 总赠送数量,
+                总收入数量: 总收入数量
             }
             await render('admin/光遇信息', {
                 ...html,
@@ -104,6 +86,30 @@ export class 我的信息 extends plugin {
         } catch (err) {
             logger.mark(err);
             e.reply('无法找到用户数据\n请先发送"光遇签到"');
+        }
+    }
+    
+    async 光遇背包(e) {
+        const 用户ID = e.user_id;
+        const 用户背包文件 = `plugins/Tlon-Sky/data/背包/${用户ID}.json`
+
+        try {
+            const 用户背包文件Data = await fs.promises.readFile(用户背包文件)
+            const 用户背包文件Json = JSON.parse(用户背包文件Data.toString())
+
+            const 蜡烛保护卡 = 用户背包文件Json[用户ID]['蜡烛保护卡']
+            let html = {
+                蜡烛保护卡: 蜡烛保护卡
+            }
+            await render('admin/光遇背包', {
+                ...html,
+            }, {
+                e,
+                scale: 1.4
+            })
+        } catch (err) {
+            logger.mark(err)
+            return e.reply('无法找到用户背包数据\n请购买任意物品后查看')
         }
     }
 }
