0d05a34708207e9bf339b05fa2602603df91e44d
Tloml-Starry <zq3620060826@163.com>
2023-10-25 23:05:10 +0800

    * 2023/10/25

--- apps/「光遇」每日功能.js
+++ apps/「光遇」每日功能.js
@@ -1,13 +1,11 @@
 import plugin from '../../../lib/plugins/plugin.js';
+import { render } from '../components/index.js'
 
 const RW_URL = 'https://api.t1qq.com/api/sky/gy/sc/tlonsky/json/mrrw.jpg';
 const JL_URL = 'https://api.t1qq.com/api/sky/gy/sc/tlonsky/json/scjl.jpg';
 const DL_URL = 'https://api.t1qq.com/api/sky/gy/sc/tlonsky/json/scdl.jpg';
 const MF_URL = 'https://api.t1qq.com/api/sky/gy/sc/json/mf.jpg';
-const FK_URL = 'https://gitee.com/Tloml-Starry/Tlon-Sky-reprint/raw/master/image/Reprint.png';
-const BigHourName = ['子', '丑', '寅', '卯', '辰', '巳', '午', '未', '申', '酉', '戌', '亥', '子'];
-const BigHourType = ['正', '初'];
-const BigMinName = ['零', '一', '二', '三', '四'];
+const FK_URL = 'https://gitee.com/Tloml-Starry/Tlon-Sky-reprint/raw/master/image/Reprint.png'
 
 export class 光遇_每日功能 extends plugin {
   constructor() {
@@ -18,57 +16,25 @@ export class 光遇_每日功能 extends plugin {
       priority: 5000,
       rule: [
         {
-          reg: /^#?(光遇今日任务|国服今日任务|今日任务|任务)$/,
-          fnc: '每日任务',
-        },
-        {
-          reg: /^#?(大蜡烛位置|大蜡位置|大蜡|DL)$/,
-          fnc: '大蜡位置',
-        },
-        {
-          reg: /^#?(季蜡|季蜡位置|JL)$/,
-          fnc: 'sky_JLWZ',
-        },
+          reg: /^#?(光遇今日任务|国服今日任务|今日任务|任务|大蜡烛位置|大蜡位置|大蜡|DL|季蜡|季蜡位置|JL|今日魔法)$/,
+          fnc: '每日'
+        }
       ],
     });
   }
-  async 每日任务(e) {
-    const msg = [
-      segment.image(MF_URL),
-      '=====今日任务=====',
-      segment.image(RW_URL),
-      '季蜡位置&大蜡烛位置',
-      segment.image(JL_URL),
-      segment.image(DL_URL),
-      '=====国服复刻=====',
-      segment.image(FK_URL)
-    ];
-    e.reply(msg);
-  }
-
-  async 大蜡位置(e) {
-    const now = new Date();
-    const hourName = BigHourName[Math.floor((now.getHours() + 1) / 2)];
-    const hourType = BigHourType[now.getHours() % 2];
-    const minName = BigMinName[Math.floor(now.getMinutes() / 15)];
-    const msg = [
-      segment.image(DL_URL),
-      `现在是长安${hourName}${hourType}${minName}刻`,
-    ];
-    e.reply(msg);
-  }
-
-  async 季蜡位置(e) {
-    const now = new Date();
-    const hour = `0${now.getHours()}`.slice(-2);
-    const minutes = `0${now.getMinutes()}`.slice(-2);
-    const msg = [
-      segment.image(JL_URL),
-      '发送时间：',
-      hour,
-      ':',
-      minutes,
-    ];
-    e.reply(msg);
+  async 每日(e) {
+    let html = {
+      魔法Url: MF_URL,
+      任务Url: RW_URL,
+      季蜡Url: JL_URL,
+      大蜡Url: DL_URL,
+      复刻Url: FK_URL
+    }
+    await render('admin/每日任务', {
+      ...html
+    }, {
+      e,
+      scale: 1.4
+    })
   }
 }
