a6f3482a071a6b0871a3e80bd4f882f09fdc7251
Tloml-Starry <zq3620060826@163.com>
2023-10-26 15:10:32 +0800

    * 2023/10/26

--- apps/「光遇」每日功能.js
+++ apps/「光遇」每日功能.js
@@ -5,7 +5,9 @@ const RW_URL = 'https://api.t1qq.com/api/sky/gy/sc/tlonsky/json/mrrw.jpg';
 const JL_URL = 'https://api.t1qq.com/api/sky/gy/sc/tlonsky/json/scjl.jpg';
 const DL_URL = 'https://api.t1qq.com/api/sky/gy/sc/tlonsky/json/scdl.jpg';
 const MF_URL = 'https://api.t1qq.com/api/sky/gy/sc/json/mf.jpg';
-const FK_URL = 'https://gitee.com/Tloml-Starry/Tlon-Sky-reprint/raw/master/image/Reprint.png'
+const FK_URL = 'https://gitee.com/Tloml-Starry/Tlon-Sky-reprint/raw/master/image/Reprint.png';
+const DB_URL = 'https://gitee.com/Tloml-Starry/Tlon-Sky-reprint/raw/master/image/代币.png';
+const JR_URL = 'https://gitee.com/Tloml-Starry/Tlon-Sky-reprint/raw/master/image/季节任务.png';
 
 export class 光遇_每日功能 extends plugin {
   constructor() {
@@ -18,6 +20,14 @@ export class 光遇_每日功能 extends plugin {
         {
           reg: /^#?(光遇今日任务|国服今日任务|今日任务|任务|大蜡烛位置|大蜡位置|大蜡|DL|季蜡|季蜡位置|JL|今日魔法)$/,
           fnc: '每日'
+        },
+        {
+          reg: /^#?(代币位置|今日代币)$/,
+          fnc: '代币'
+        },
+        {
+          reg: /^#?季节任务$/,
+          fnc: '季任'
         }
       ],
     });
@@ -36,5 +46,13 @@ export class 光遇_每日功能 extends plugin {
       e,
       scale: 1.4
     })
+  }
+
+  async 代币(e) {
+    e.reply(segment.image(DB_URL))
+  }
+
+  async 季任(e) {
+    e.reply(segment.image(JR_URL))
   }
 }
