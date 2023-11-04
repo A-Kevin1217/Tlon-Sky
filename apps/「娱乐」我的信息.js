457a67f5f80889ab2a418e41f667b83456c12be1
Tloml-Starry <zq3620060826@163.com>
2023-11-04 21:07:54 +0800

    * 2023/11/04

--- apps/「娱乐」我的信息.js
+++ apps/「娱乐」我的信息.js
@@ -24,8 +24,15 @@ export class 我的信息 extends plugin {
     }
 
     async 光遇信息(e) {
+        const result_白 = await 白蜡排行数据();
+        logger.mark('白蜡排行写入成功', result_白);
+        const result_季 = await 季蜡排行数据();
+        logger.mark('季蜡排行写入成功', result_季);
         const 用户ID = e.user_id;
         const 用户文件 = `plugins/Tlon-Sky/data/Sky签到/${用户ID}.json`;
+
+        const 白蜡排名 = getUserRank(用户ID);
+        const 季蜡排名 = getUserRank_(用户ID);
 
         try {
             const 用户文件Data = await fs.promises.readFile(用户文件);
@@ -74,7 +81,9 @@ export class 我的信息 extends plugin {
                 平均抢: 平均抢,
                 平均被抢: 平均被抢,
                 总赠送数量: 总赠送数量,
-                总收入数量: 总收入数量
+                总收入数量: 总收入数量,
+                白蜡排名: 白蜡排名,
+                季蜡排名: 季蜡排名
             }
             await render('admin/光遇信息', {
                 ...html,
@@ -88,7 +97,7 @@ export class 我的信息 extends plugin {
             e.reply('无法找到用户数据\n请先发送"光遇签到"');
         }
     }
-    
+
     async 光遇背包(e) {
         const 用户ID = e.user_id;
         const 用户背包文件 = `plugins/Tlon-Sky/data/背包/${用户ID}.json`
@@ -126,4 +135,94 @@ const rodom = async function () {
     }
     let imgs = listImg.length == 1 ? listImg[0] : listImg[lodash.random(0, listImg.length - 1)]
     return imgs
+}
+
+const 白蜡排行数据 = async function () {
+
+    const files = fs.readdirSync(用户位置); // 读取目录下的所有文件
+    const 用户数量 = files.length;
+    const ranking = [];
+
+    // 遍历所有文件
+    files.forEach((fileName) => {
+        if (fileName.endsWith('.json')) {
+            const userId = fileName.split('.')[0];
+            const filePath = path.join(用户位置, fileName);
+            const fileData = fs.readFileSync(filePath);
+            const data = JSON.parse(fileData.toString());
+            const level = data[userId]?.白蜡 || 0;
+            const nickname = data[userId]?.昵称 || '未读取';
+
+            ranking.push({ nickname, level, userId });
+        }
+    });
+
+    ranking.sort((a, b) => b.level - a.level);
+
+    const topTen = ranking.slice(0, 用户数量);
+    // 将排行信息储存为 JSON 文件
+    const jsonRanking = JSON.stringify(topTen, null, 2); // 格式化 JSON，使其更易读
+
+    fs.writeFileSync('plugins/Tlon-Sky/data/排行榜/白蜡.json', jsonRanking);
+}
+
+const 季蜡排行数据 = async function () {
+    const files = fs.readdirSync(用户位置);
+    const 用户数量 = files.length;
+    const ranking = [];
+
+    files.forEach((fileName) => {
+        if (fileName.endsWith('.json')) {
+            const userId = fileName.split('.')[0];
+            const filePath = path.join(用户位置, fileName);
+            const fileData = fs.readFileSync(filePath);
+            const data = JSON.parse(fileData.toString());
+            const level = data[userId]?.季蜡 || 0;
+            const nickname = data[userId]?.昵称 || '未读取';
+
+            ranking.push({ nickname, level, userId });
+        }
+    });
+
+    ranking.sort((a, b) => b.level - a.level);
+
+    const topTen = ranking.slice(0, 用户数量);
+
+    const jsonRanking = JSON.stringify(topTen, null, 2);
+
+    fs.writeFileSync('plugins/Tlon-Sky/data/排行榜/季蜡.json', jsonRanking);
+}
+
+async function getUserRank(userId) {
+    const 排行信息_白 = 'plugins/Tlon-Sky/data/排行榜/白蜡.json'
+    const 排行信息Data_白 = await fs.promises.readFile(排行信息_白)
+    const 排行信息Json_白 = JSON.parse(排行信息Data_白.toString())
+    // 根据 level 属性对 JSON 数据进行排序
+    const sortedData = 排行信息Json_白.sort((a, b) => b.level - a.level);
+
+    // 遍历排序后的数据，查找匹配的 userId
+    for (let i = 0; i < sortedData.length; i++) {
+        if (sortedData[i].userId === userId) {
+            return i + 1; // 返回排名（加1是因为排名从1开始）
+        }
+    }
+
+    return -1; // 如果未找到匹配的 userId，则返回 -1 表示未找到
+}
+
+async function getUserRank_(userId) {
+    const 排行信息_季 = 'plugins/Tlon-Sky/data/排行榜/季蜡.json'
+    const 排行信息Data_季 = await fs.promises.readFile(排行信息_季)
+    const 排行信息Json_季 = JSON.parse(排行信息Data_季.toString())
+    // 根据 level 属性对 JSON 数据进行排序
+    const sortedData = 排行信息Json_季.sort((a, b) => b.level - a.level);
+
+    // 遍历排序后的数据，查找匹配的 userId
+    for (let i = 0; i < sortedData.length; i++) {
+        if (sortedData[i].userId === userId) {
+            return i + 1; // 返回排名（加1是因为排名从1开始）
+        }
+    }
+
+    return -1; // 如果未找到匹配的 userId，则返回 -1 表示未找到
 }
