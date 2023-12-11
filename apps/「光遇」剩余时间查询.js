f61e3ea942981d0f482da6395c5ecae1621a76b1
Tloml-Starry <zq3620060826@163.com>
2023-12-11 17:41:07 +0800

    * 2023/12/11

--- apps/「光遇」剩余时间查询.js
+++ apps/「光遇」剩余时间查询.js
@@ -19,30 +19,41 @@ export class 光遇_剩余时间 extends plugin {
     })
   }
   async 季节剩余(e) {
-    let 季节指定时间 = new Date('2024-01-10 23:59:59').getTime();
+    const END_TIME = new Date('2024-01-10 23:59:59').GET_TIME();
     let msg = '';
 
     function countdown() {
-      let 现在时间 = Date.now();
-      let 季节剩余时间_毫秒 = 季节指定时间 - 现在时间;
+      const GET_TIME = Date.now();
+      const MILLISECOND = END_TIME - GET_TIME;
 
-      if (季节剩余时间_毫秒 <= 0) {
+      if (MILLISECOND <= 0) {
         msg = '归巢季已结束！请等待下个季节到来.';
         return;
       }
 
-      let 季节剩余时间_天 = Math.floor(季节剩余时间_毫秒 / (24 * 60 * 60 * 1000));
-      let 季节剩余时间_小时 = Math.floor((季节剩余时间_毫秒 % (24 * 60 * 60 * 1000)) / (60 * 60 * 1000));
-      let 季节剩余时间_分钟 = Math.floor((季节剩余时间_毫秒 % (60 * 60 * 1000)) / (60 * 1000));
-      let 季节剩余时间_秒 = Math.floor((季节剩余时间_毫秒 % (60 * 1000)) / 1000);
+      const DAY = Math.floor(MILLISECOND / (24 * 60 * 60 * 1000));
+      const HOUR = Math.floor((MILLISECOND % (24 * 60 * 60 * 1000)) / (60 * 60 * 1000));
+      const MINUTE = Math.floor((MILLISECOND % (60 * 60 * 1000)) / (60 * 1000));
+      const SECOND = Math.floor((MILLISECOND % (60 * 1000)) / 1000);
 
-      msg = `距离归巢季结束还剩\n${季节剩余时间_天} 天 ${季节剩余时间_小时} 小时 ${季节剩余时间_分钟} 分钟 ${季节剩余时间_秒} 秒\n截止至2024-01-10 23:59:59`;
+      msg = [
+        `距离归巢季结束还剩\n`,
+        `${DAY} 天 ${HOUR} 小时 ${MINUTE} 分钟 ${SECOND} 秒\n`,
+        `截止至2024-01-10 23:59:59\n`,
+        `季蜡还可获得\n`,
+        `[有季卡]：${DAY * 6}季蜡\n`,
+        `[无季卡]：${DAY * 5}季蜡\n`,
+        `本季节毕业需：396季蜡\n`,
+        `[有季卡]毕业需：${((396 - 30) / 6).toFixed(1)}\n`,
+        `[无季卡]毕业需：${(396 / 5).toFixed(1)}`
+      ]
       setTimeout(countdown, 1000);
     }
     countdown();
     await this.reply(msg, true);
     return;
   }
+  
   async 季节结束时间(e) {
     const imgreply = 'plugins/Tlon-Sky/resource/统计及其他/季节结束时间.png'
     await this.reply([imgreply ? segment.image(imgreply) : ""], true)
