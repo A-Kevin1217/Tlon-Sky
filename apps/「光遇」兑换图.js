613f583945dafd9f4382b41983c19a8ae9566213
Tloml-Starry <zq3620060826@163.com>
2024-01-15 13:32:00 +0800

    * 2024/01/15

--- apps/「光遇」兑换图.js
+++ apps/「光遇」兑换图.js
@@ -1,5 +1,10 @@
-export class 光遇_兑换图 extends plugin {
-  constructor () {
+import fs from 'fs';
+
+const PERMANENT_REGEX = /^(#|\/)?(晨岛|云野|雨林|峡谷|霞谷|暮土|禁阁)兑换图$/
+const SEASON_REGEX = /^(#|\/)?(AURORA|表演|风行|感恩|归巢|归属|九色鹿|梦想|魔法|破晓|潜海|圣岛|拾光|小王子|夜行|音韵|预言|重组|追光|追忆|欧若拉|集结|凌冬)兑换图$/
+const PICTURE_RESOURCE = 'plugins/Tlon-Sky/resource/Picture'
+export class SKY_EXCHANGE_PICTURE extends plugin {
+  constructor() {
     super({
       name: '[Tlon-Sky]光遇:兑换图',
       dsc: '光遇兑换图',
@@ -7,37 +12,40 @@ export class 光遇_兑换图 extends plugin {
       priority: 5000,
       rule: [
         {
-          reg: /^(#|\/)?季节兑换图$/,
-          fnc: '季节兑换图'
-        },{
-          reg: /^(#|\/)?常驻兑换图$/,
-          fnc: '常驻兑换图'
-        },{
-          reg: /^(#|\/)?往期复刻$/,
-          fnc: '往期复刻图'
+          reg: PERMANENT_REGEX,
+          fnc: 'PERMANENT_EXCHANGE_PICTURE'
+        },
+        {
+          reg: SEASON_REGEX,
+          fnc: 'SEASON_EXCHANGE_PICTURE'
+        },
+        {
+          reg: /^(#|\/)?(复刻兑换图|国服复刻)$/,
+          fnc: 'RETURN_EXCHANGE_PICTURE'
         }
       ]
     })
   }
-  async 季节兑换图(e) {
-    const imgreply = 'plugins/Tlon-Sky/resource/help/季节兑换图.png'
-    e.reply(
-      imgreply ? segment.image(imgreply) : ""
-      )
-    return true;
+
+  async PERMANENT_EXCHANGE_PICTURE(e) {
+    const match = e.msg.match(PERMANENT_REGEX);
+    const imgreply = `${PICTURE_RESOURCE}/Exchange picture/Permanent/${match[2]}.png`;
+    if (!fs.existsSync(imgreply)) { return e.reply('无此常驻兑换图，请检查名称是否正确\n晨岛丨云野丨雨林丨霞谷丨暮土丨禁阁'); }
+    return e.reply(segment.image(imgreply), true)
   }
-  async 常驻兑换图(e) {
-    const imgreply = 'plugins/Tlon-Sky/resource/help/常驻兑换图.png'
-    e.reply(
-      imgreply ? segment.image(imgreply) : ""
-    )
-    return true;
+
+  async SEASON_EXCHANGE_PICTURE(e) {
+    const match = e.msg.match(PERMANENT_REGEX);
+    let season = match[2];
+    const seasonMapping = { '欧若拉': 'AURORA', '集结': '重组', '凌冬': '音韵' };
+    if (seasonMapping.hasOwnProperty(season)) { season = seasonMapping[season]; }
+    const imgreply = `${PICTURE_RESOURCE}/Exchange picture/Season/${season}.png`;
+    if (!fs.existsSync(imgreply)) { return e.reply('无此季节兑换图，请检查名称是否正确'); }
+    return e.reply(segment.image(imgreply), true)
   }
-  async 往期复刻图(e) {
-    const imgreply = 'plugins/Tlon-Sky/resource/help/往期复刻记录.png'
-    e.reply(
-      imgreply ? segment.image(imgreply) : ""
-    )
-    return true;
+
+  async RETURN_EXCHANGE_PICTURE(e) {
+    e.reply('仅供参考,以正式上线为准', false, { recallMsg: 10 })
+    return e.reply(segment.image('https://gitee.com/Tloml-Starry/Tlon-Sky-reprint/raw/master/image/Reprint.png'), true)
   }
 }
