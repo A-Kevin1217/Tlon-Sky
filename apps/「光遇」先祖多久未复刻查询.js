7356d2b6c3e044a0b77bc83447f79f864b39e8dd
Tloml-Starry <zq3620060826@163.com>
2024-02-20 12:28:22 +0800

    * 2024/02/20

--- apps/「光遇」先祖多久未复刻查询.js
+++ apps/「光遇」先祖多久未复刻查询.js
@@ -77,7 +77,7 @@ const seasons = {
     { name: '放松日浴者', date: new Date('2023-11-20') },
     { name: '鸣谢收藏家', date: new Date('2023-10-03') },
     { name: '固执登山者', date: new Date('2023-04-10') },
-    { name: '热血运动员', date: new Date('2023-09-11') },
+    { name: '热血运动员', date: new Date('2024-02-26') },
     { name: '内秀书虫', date: new Date('2022-10-25') },
     { name: '母语者', date: new Date('2022-08-01') }
   ],
@@ -112,8 +112,8 @@ const seasons = {
   '风行': [
     { name: '风铃修补匠', date: new Date('2023-12-04') },
     { name: '天才建筑者', date: new Date('2024-01-01') },
-    { name: '风行领航员', data: new Date('2024-01-29') }
+    { name: '风行领航员', date: new Date('2024-01-29') }
   ]
 };
 
-// { name: '', data: new Date('2024-01-01') }
+// { name: '', date: new Date('2024-01-01') }
