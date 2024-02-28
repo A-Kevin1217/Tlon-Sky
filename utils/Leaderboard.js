25eae1712ab04eea1f6a1b6a3c59b332cd13e6a9
Tloml-Starry <zq3620060826@163.com>
2024-02-28 22:10:03 +0800

    * 2024/02/28 - 优化细节

--- utils/Leaderboard.js
+++ utils/Leaderboard.js
@@ -1,49 +1,50 @@
 import path from 'path';
 import fs from 'fs/promises';
 
-const Userfile = 'plugins/Tlon-Sky/data/Sky签到';
+const userFile = 'plugins/Tlon-Sky/data/Sky签到';
+const rankingKeys = ['白蜡', '季蜡', '亏损', '赚取', '抢蜡烛次数', '被抢次数', '连续签到天数', '累计签到天数'];
 
-export async function Leaderboard() {
+const readJsonFile = async (filePath) => {
     try {
-        const Allfile = await fs.readdir(Userfile);
-        const UserNumber = Allfile.length;
+        const fileData = await fs.readFile(filePath);
+        return JSON.parse(fileData.toString());
+    } catch (error) {
+        console.error('Error reading file:', error);
+        return null;
+    }
+};
+
+const sortAndWriteToFile = async (array, fileName) => {
+    array.sort((a, b) => b.level - a.level);
+    const topTen = array.slice(0, array.length);
+    const jsonRanking = JSON.stringify(topTen, null, 2);
+    await fs.writeFile(`plugins/Tlon-Sky/data/排行榜/${fileName}.json`, jsonRanking);
+};
+
+export async function leaderboard() {
+    try {
+        const allFiles = await fs.readdir(userFile);
+        const userNumber = allFiles.length;
         const ranking = [];
-        const rankingKeys = ['白蜡', '季蜡', '亏损', '赚取', '抢蜡烛次数', '被抢次数', '连续签到天数', '累计签到天数'];
 
-        await Promise.all(Allfile.map(async (fileName) => {
+        await Promise.all(allFiles.map(async (fileName) => {
             if (fileName.endsWith('.json')) {
-                let userId = fileName.split('.')[0]
-                if (userId.length > 20) {
-                    userId
-                } else {
-                    userId = parseFloat(userId)
+                const userId = fileName.split('.')[0].length > 20 ? fileName.split('.')[0] : parseFloat(fileName.split('.')[0]);
+                const filePath = path.join(userFile, fileName);
+                const data = await readJsonFile(filePath);
+                if (data) {
+                    const { 昵称: nickname = '未读取', 头像: headShot = 3620060826, ...restData } = data;
+                    rankingKeys.forEach((key) => {
+                        const level = restData[key] || 0;
+                        ranking.push({ nickname, level, userId, headShot });
+                    });
                 }
-                const filePath = path.join(Userfile, fileName);
-                const fileData = await fs.readFile(filePath);
-                const data = JSON.parse(fileData.toString());
-                const nickname = data?.昵称 || '未读取';
-                const Head_shot = data?.头像 || 3620060826;
-
-                const rankingData = {};
-                rankingKeys.forEach((key) => {
-                    const level = data?.[key] || 0;
-                    rankingData[key] = { nickname, level, userId, Head_shot };
-                });
-
-                ranking.push(rankingData);
             }
         }));
 
-        const sortAndWriteToFile = async (array, fileName) => {
-            array.sort((a, b) => b.level - a.level);
-            const topTen = array.slice(0, UserNumber);
-            const jsonRanking = JSON.stringify(topTen, null, 2);
-            await fs.writeFile(`plugins/Tlon-Sky/data/排行榜/${fileName}.json`, jsonRanking);
-        };
-
-        rankingKeys.forEach(async (key) => {
-            await sortAndWriteToFile(ranking.map(data => data[key]), key);
-        });
+        await Promise.all(rankingKeys.map(async (key) => {
+            await sortAndWriteToFile(ranking.filter(data => data[key]), key);
+        }))
     } catch (error) {
         console.error('Error:', error);
     }
