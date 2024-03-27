fa1edf88da9e2d54fec61717d4f4a46abd3517b4
Tloml-Starry <zq3620060826@163.com>
2024-03-27 15:37:12 +0800

    * 2024/03/27 - 更新先祖多久未复刻时间，优化

--- apps/「光遇」先祖多久未复刻查询.js
+++ apps/「光遇」先祖多久未复刻查询.js
@@ -25,12 +25,12 @@ export class SKY extends plugin {
   async snrd(e) {
     const SEASON_NAME = (e.msg.match(REGEX))[2]
 
-    let msg = '数据更新时间：2024-02-29\n此表不计入集体复刻\n'
 
     const SEASON_DATA = await (await fetch('https://gitee.com/Tloml-Starry/resources/raw/master/resources/json/先祖多久未复刻.json')).json()
 
     if (!SEASON_DATA[SEASON_NAME]) return e.reply('不存在该季节,请输入以下季节名：\n感恩丨追光丨归属丨音韵\n魔法丨圣岛丨预言丨梦想\n集结丨小王子丨风行')
 
+    let msg = `数据更新时间：${SEASON_DATA['UPDATE TIME']}\n此表不计入集体复刻\n`
     for (const role of SEASON_DATA[SEASON_NAME]) {
       const dayDiff = this.getDayDiff(new Date(role.date))
       msg += `${role.name}已[ ${dayDiff} ]天！未复刻\n`
