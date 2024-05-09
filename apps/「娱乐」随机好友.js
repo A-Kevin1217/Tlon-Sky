21e81602e76c792a656b54728f8f99d177368a22
Tloml-Starry <zq3620060826@163.com>
2024-05-09 17:02:09 +0800

    ✅随机好友重构完成

--- apps/「娱乐」随机好友.js
+++ apps/「娱乐」随机好友.js
@@ -1,102 +1,94 @@
 import fs from 'fs';
-// import { GD, SD } from '../utils/db.js';
 
-// const FriendCodeFile = 'plugins/Tlon-Sky/data/FriendCodeFile.json'
-// const FriendRemarksFile = 'plugins/Tlon-Sky/data/FriendRemarks.json'
-// const FriendCodeCDFile = 'plugins/Tlon-Sky/data/FriendCodeCD'
-// if (!fs.existsSync(FriendCodeFile)) {
-//     const Code = []
-//     SD(FriendCodeFile, Code); SD(FriendRemarksFile, Code)
-// }
-// const CoolingTime = 1 * 60 * 60 * 1000;
+const RFF = 'plugins/Tlon-Sky/data/Random friends.json'
+const CDF = 'plugins/Tlon-Sky/data/Random friends CD.json'
+if (!fs.existsSync(RFF)) { SD(RFF, []); SD(CDF, {}) }
 
-export class RandomFriend extends plugin {
+const R = /^(#|\/)?存入盲盒(.*)\*(国|国际|测试)服$/i
+
+export class SKY extends plugin {
     constructor() {
         super({
             name: '[Tlon-Sky]光遇:随机好友',
             event: 'message',
-            priority: 5000,
+            priority: 1,
             rule: [
-                {
-                    reg: /^(#|\/)?随机(好友|盲盒)$/,
-                    fnc: 'RandomFriend'
-                },
-                {
-                    reg: /^(#|\/)?存入盲盒(.*)\*(.*)$/,
-                    fnc: 'SaveFriendCode'
-                }
+                { reg: R, fnc: 'F1' },
+                { reg: /^(#|\/)?随机好友$/, fnc: 'F2' }
             ]
         })
     }
 
-    // async RandomFriend(e) {
-    //     const UserCdFile = `${FriendCodeCDFile}/${e.user_id}.json`
-    //     if (!fs.existsSync(UserCdFile)) {
-    //         const Cd = {
-    //             CD: Date.now() - CoolingTime
-    //         }
-    //         SD(UserCdFile, Cd)
-    //     }
-    //     const UserCdData = GD(UserCdFile)
-    //     const UserCd = UserCdData['CD']
-    //     if (Date.now() - UserCd < CoolingTime) {
-    //         const RemainingTimestamp = CoolingTime - (Date.now() - UserCd);
-    //         if (RemainingTimestamp > 0) {
-    //             const hour = Math.floor(RemainingTimestamp / (60 * 60 * 1000));
-    //             const minutes = Math.floor((RemainingTimestamp % (60 * 60 * 1000)) / (60 * 1000));
-    //             const second = Math.floor((RemainingTimestamp % (60 * 1000)) / 1000);
+    async F1(e) {
+        const UID = e.user_id
+        const GET_CONTENT = e.msg.match(R)
+        const CODE = GET_CONTENT[2]
+        const SERVER = GET_CONTENT[3]
 
-    //             const EndTimestamp = Date.now() + RemainingTimestamp;
-    //             const EndTime = new Date(EndTimestamp).toLocaleString();
+        if (!/^[0-9A-Z]{4}-[0-9A-Z]{4}-[0-9A-Z]{4}$/.test(CODE)) return e.reply((e.adapter === 'QQBot') ? [
+            '# 格式错误！请按照以下格式添加',
+            '> 存入盲盒[好友代码]\*[服务器]',
+            '如：存入盲盒ABCD-EFGH-IJKL\*国服',
+            Bot.Button([[{ label: '重新输入', data: `存入盲盒${CODE}*${LABELS}` }]])
+        ] : [
+            '格式错误！请按照以下格式添加\n' +
+            '存入盲盒[好友代码]*[服务器]\n' +
+            '如：存入盲盒ABCD-EFGH-IJKL*国服'
+        ])
 
-    //             if (hour === 0) {
-    //                 return e.reply(`盲盒CD中\n请等待 ${minutes} 分钟 ${second} 秒后再开盲盒！\nCD结束时间：${EndTime}`);
-    //             } else if (minutes === 0) {
-    //                 return e.reply(`盲盒CD中\n请等待 ${second} 秒后再开盲盒！\nCD结束时间：${EndTime}`);
-    //             } else {
-    //                 return e.reply(`盲盒CD中\n请等待 ${hour} 小时 ${minutes} 分钟 ${second} 秒后再开盲盒！\nCD结束时间：${EndTime}`)
-    //             }
-    //         }
-    //     }
-    //     const FriendCodeData = GD(FriendCodeFile);
-    //     const FriendRemarksData = GD(FriendRemarksFile);
-    //     const FriendCodeNumber = FriendCodeData.length;
+        const RFD = await GD1()
+        RFD.push({ UID: UID, C: CODE, S: SERVER })
+        SD(RFF, RFD)
 
-    //     if (FriendCodeNumber === 0) {
-    //         return e.reply('好友盲盒用完啦，您可以使用指令\n存入盲盒[好友代码]*[备注]\n来存入你的盲盒');
-    //     }
+        return e.reply((e.adapter === 'QQBot') ? [
+            '# 存入成功!',
+            '***',
+            `# 好友码: ${CODE}`,
+            `> 服务器: ${SERVER}服`
+        ] : [
+            segment.at(UID),
+            '存入成功！\n' +
+            `好友码: ${CODE}\n` +
+            `服务器: ${SERVER}服`
+        ])
+    }
 
-    //     const RandomNumber = Math.floor(Math.random() * FriendCodeNumber);
-    //     const FriendCode = FriendCodeData.splice(RandomNumber, 1)[0];
-    //     const FriendRemarks = FriendRemarksData.splice(RandomNumber, 1)[0];
+    async F2(e) {
+        const UID = e.user_id
+        const CDD = await GD2()
 
-    //     UserCdData['CD'] = Date.now()
-    //     SD(FriendCodeFile, FriendCodeData);
-    //     SD(FriendRemarksFile, FriendRemarksData);
-    //     SD(UserCdFile, UserCdData)
-    //     e.reply(`来咯~请及时添加好友哦~\n好友代码：${FriendCode}\n对方备注：${FriendRemarks}\n防骗提示：盲盒来源网友，请谨防受骗`, true);
-    //     return e.reply(`如果您不需要这个盲盒请添加回去指令：\n存入盲盒${FriendCode}*${FriendRemarks}`, true);
-    // }
+        if (CDD[UID] === GCD()) return e.reply('今日已获取过盲盒，请与明日再来')
 
-    // async SaveFriendCode(e) {
-    //     const UserMsg = e.msg.match(/^(#|\/)?存入盲盒(.*)\*(.*)/)
-    //     const FriendCode = UserMsg[2]
-    //     const FriendRemarks = UserMsg[3]
-    //     const regex = /^[0-9A-Za-z]{4}-[0-9A-Za-z]{4}-[0-9A-Za-z]{4}$/;
-    //     const isValidFormat = regex.test(FriendCode);
-    //     if (!isValidFormat) {
-    //         return e.reply('好友代码不符合格式要求\n示例：\n存入盲盒xxxx-xxxx-xxxx*国服|备注小光|平常晚上在线');
-    //     }
+        const RFD = await GD1()
+        if (RFD.length === 0) return e.reply((e.adapter === 'QQBot') ? ['盲盒库已无盲盒，请添加一些再来吧~', Bot.Button([[{ label: '添加', data: '存入盲盒[好友代码]*[服务器]' }]])] : ['盲盒库已无盲盒，请添加一些再来吧~'])
+        const RANDOM_FRIENDS_DATA = RFD[Math.floor(Math.random() * RFD.length)];
 
-    //     const FriendCodeData = GD(FriendCodeFile);
-    //     const FriendRemarksData = GD(FriendRemarksFile);
+        RFD = RFD.filter(item => item !== RANDOM_FRIENDS_DATA)
+        CDD[UID] = GCD()
 
-    //     FriendCodeData.push(FriendCode);
-    //     FriendRemarksData.push(FriendRemarks);
+        SD(RFF, RFD)
+        SD(CDF, CDD)
 
-    //     SD(FriendCodeFile, FriendCodeData);
-    //     SD(FriendRemarksFile, FriendRemarksData);
+        e.reply((e.adapter === 'QQBot') ? [
+            '# 您的盲盒~',
+            '***',
+            `# 好友代码: ${RANDOM_FRIENDS_DATA['C']}`,
+            `> 服务器: ${RANDOM_FRIENDS_DATA['S']}服`,
+            Bot.Button([[{ label: '不需要？放回去', data: `存入盲盒${RANDOM_FRIENDS_DATA['C']}*${RANDOM_FRIENDS_DATA['S']}服` }]])
+        ] : [
+            '您的盲盒~' +
+            `好友代码: ${RANDOM_FRIENDS_DATA['C']}` +
+            `服务器: ${RANDOM_FRIENDS_DATA['S']}服`
+        ])
+    }
+}
 
-    //     return e.reply('好友代码已存入盲盒！', true);
-    // }
+function SD(FILE, DATA) { fs.writeFileSync(FILE, JSON.stringify(DATA, null, 4), 'utf8') }
+async function GD1() { return JSON.parse(fs.readFileSync(RFF, 'utf8')) }
+async function GD2() { return JSON.parse(fs.readFileSync(CDF, 'utf8')) }
+function GCD() {
+    const date = new Date();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    return `${month}-${day}`;
 }
