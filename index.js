f5388dd424775245cd87ada74b3c85bc6128ed8e
Tloml-Starry <zq3620060826@163.com>
2024-06-27 23:15:50 +0800

    🔧增加载入提示，修复路径

--- index.js
+++ index.js
@@ -39,6 +39,7 @@ for (let i in file) {
     logger.error(ret[i].reason)
     continue
   }
+  logger.mark(logger.green(`${name} 已载入`))
   apps[name] = ret[i].value[Object.keys(ret[i].value)[0]]
 }
 export { apps }
