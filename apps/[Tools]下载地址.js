a71bd0f966b23782a2a3f9be0fbed3f68d357ef3
Tloml-Starry <zq3620060826@163.com>
2024-08-13 02:18:11 +0800

    update

--- apps/[Tools]下载地址.js
+++ apps/[Tools]下载地址.js
@@ -1,23 +1,28 @@
-/** 官服 */
-const URL_1 = 'https://sky.163.com'
-/** 4399服 */
-const URL_2 = 'http://a.4399.cn/mobile/112700.html'
-/** OPPO服 */
-const URL_3 = 'https://game.oppomobile.com'
-/** BiliBili服 */
-const URL_4 = 'https://www.biligame.com/detail/?id=101661'
-/** VIVO服 */
-const URL_5 = 'http://h5.appstore.vivo.com.cn/#/details?appId=2902164'
-/** 九游服 */
-const URL_6 = 'https://a.9game.cn/skygy'
-/** 华为服 */
-const URL_7 = 'https://appgallery.huawei.com/app/C100685413'
-/** 小米服 */
-const URL_8 = 'https://m.app.mi.com/#page=detail&id=819119'
-/** 应用宝服 */
-const URL_9 = 'https://sj.qq.com/appdetail/com.tencent.tmgp.eyou.eygy'
-/** 国际服 */
-const URL_10 = 'https://www.thatskygame.com'
+/**
+ * 光遇各渠道下载链接  
+ * [0] 官服  
+ * [1] 4399  
+ * [2] OPPO  
+ * [3] BiliBili  
+ * [4] VIVO  
+ * [5] 九游  
+ * [6] 华为  
+ * [7] 小米  
+ * [8] 应用宝  
+ * [9] 国际服
+ */
+const link = [
+    'https://sky.163.com',
+    'http://a.4399.cn/mobile/112700.html',
+    'https://game.oppomobile.com',
+    'https://www.biligame.com/detail/?id=101661',
+    'http://h5.appstore.vivo.com.cn/#/details?appId=2902164',
+    'https://a.9game.cn/skygy',
+    'https://appgallery.huawei.com/app/C100685413',
+    'https://m.app.mi.com/#page=detail&id=819119',
+    'https://sj.qq.com/appdetail/com.tencent.tmgp.eyou.eygy',
+    'https://www.thatskygame.com'
+]
 export class SKY extends plugin {
     constructor() {
         super({
@@ -25,48 +30,38 @@ export class SKY extends plugin {
             dsc: 'Tlon-Sky',
             event: 'message',
             priority: 1,
-            rule: [
-                {
-                    reg: /^(#|\/)?(光遇下载(链接)?|下载光遇)$/,
-                    fnc: 'F1'
-                }
-            ]
+            rule: [{ reg: /^(#|\/)?(光遇下载(链接)?|下载光遇)$/, fnc: 'gameDownloadLink' }]
         })
     }
-    async F1(e) {
-        let REPLY = []
+
+    async gameDownloadLink(e) {
+        let msg = []
         if (e.adapter === 'QQBot') {
-            REPLY = [
+            function bd(label, link) {
+                return { label, link }
+            }
+            msg = [
                 '# 请点击下方按钮跳转下载',
-                Bot.Button([[
-                    { label: '官服', link: URL_1 },
-                    { label: '国际服', link: URL_10 }
-                ], [
-                    { label: '4399', link: URL_2 },
-                    { label: 'OPPO', link: URL_3 },
-                    { label: 'BiliBili', link: URL_4 },
-                    { label: 'VIVO', link: URL_5 }
-                ], [
-                    { label: '九游', link: URL_6 },
-                    { label: '华为', link: URL_7 },
-                    { label: '小米', link: URL_8 },
-                    { label: '应用宝', link: URL_9 }
-                ]])
+                Bot.Button([
+                    [bd('官服', link[0]), bd('4399', link[1])]
+                    [bd('OPPO', link[2]), bd('BiliBili', link[3]), bd('VIVO', link[4]), bd('九游', link[5])]
+                    [bd('华为', link[6]), bd('小米', link[7]), bd('应用宝', link[8]), bd('国际服', link[9])]
+                ])
             ]
         } else {
-            REPLY = [
-                `官服：${URL_1}`,
-                `\n4399渠道服：${URL_2}`,
-                `\nOPPO渠道服：${URL_3}`,
-                `\nBiliBili渠道服：${URL_4}`,
-                `\nVIVO渠道服：${URL_5}`,
-                `\n九游渠道服：${URL_6}`,
-                `\n华为渠道服：${URL_7}`,
-                `\n小米渠道服：${URL_8}`,
-                `\n应用宝渠道服：${URL_9}`,
-                `\n国际服：${URL_10}`
+            msg = [
+                `官服: ${link[1]}`,
+                `\r4399渠道服: ${link[2]}`,
+                `\rOPPO渠道服: ${link[3]}`,
+                `\rBiliBili渠道服: ${link[4]}`,
+                `\rVIVO渠道服: ${link[5]}`,
+                `\r九游渠道服: ${link[6]}`,
+                `\r华为渠道服: ${link[7]}`,
+                `\r小米渠道服: ${link[8]}`,
+                `\r应用宝渠道服: ${link[9]}`,
+                `\r国际服: ${link[10]}`
             ]
         }
-        return e.reply(REPLY)
+        return e.reply(msg)
     }
 }
