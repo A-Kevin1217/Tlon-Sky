9007e6b8d6f6b3e0136e44c6d92e414df99ea38e
Tloml-Starry <zq3620060826@163.com>
2024-08-28 03:00:27 +0800

    add: 老奶奶干饭提醒 || 使用: 开启/关闭老奶奶干饭提醒

---
+++ apps/[Task]老奶奶干饭提醒.js
@@ -0,0 +1,69 @@
+import common from "../../../lib/common/common.js";
+import {
+    getPushData,
+    getPushTextData,
+    storagePushData
+} from '../function/function.js';
+
+const regex = /^[#\/]?(开启|关闭)老奶奶干饭提醒$/
+export class Ts extends plugin {
+    constructor() {
+        super({
+            name: '[Ts]老奶奶干饭提醒',
+            dsc: '老奶奶干饭提醒',
+            event: 'message',
+            priority: 1,
+            rule: [
+                { reg: regex, fnc: 'Ts' }
+            ]
+        })
+        this.task = {
+            naem: '[定时推送]老奶奶干饭提醒',
+            fnc: () => this.Push(),
+            cron: '0 55 7,9,11,15,17,19 * * *'
+        }
+    }
+
+    async Ts(e) {
+        if (!e.isGroup || !e.member.is_admin && !e.isMaster) return false
+        const ID = e.group_id
+        const pushData = await getPushData()
+
+        const [, openOrClose] = e.msg.match(regex)
+
+        if (openOrClose === '开启') {
+            pushData['老奶奶干饭'].push(ID)
+        } else {
+            pushData['老奶奶干饭'] = pushData['老奶奶干饭'].filter(a => a !== ID)
+        }
+
+        storagePushData(pushData)
+        e.reply([`已[${openOrClose}]本群老奶奶干饭提醒`])
+    }
+
+    async Push() {
+        const pushData = await getPushData()
+        const textData = await getPushTextData()
+
+        const { atAll, text, image } = textData['老奶奶干饭']
+
+        let message = []
+
+        if (atAll) {
+            message.push(segment.at('all'))
+        }
+
+        if (text) {
+            message.push(text)
+        }
+
+        if (image) {
+            message.push(segment.image(image))
+        }
+
+        for (const ID of pushData['老奶奶干饭']) {
+            Bot.pickGroup(ID).sendMsg(message);
+            common.sleep(1000)
+        }
+    }
+}
---
+++ function/function.js
@@ -0,0 +1,22 @@
+import fs from 'fs'; import path from 'path'; import Yaml from 'yaml';
+
+export const pluginPath = path.join(path.resolve(), 'plugins', 'Tlon-Sky');
+const otherFilePath = {
+    push: path.join(pluginPath, 'config', 'push.yaml'),
+    text: path.join(pluginPath, 'config', 'text.json')
+};
+
+/** 读取推送配置数据 */
+export async function getPushData() {
+    return Yaml.parse(fs.readFileSync(otherFilePath['push'], 'utf-8'))
+}
+
+/** 读取推送文案配置数据 */
+export async function getPushTextData() {
+    return await JSON.parse(fs.readFileSync(FILE_PATH['text'], 'utf8'))
+}
+
+/** 存储推送配置数据 */
+export function storagePushData(data) {
+    fs.writeFileSync(otherFilePath['push'], Yaml.stringify(data), 'utf8')
+}
