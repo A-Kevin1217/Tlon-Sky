923561cabd3127f49b0873cded0dc4693819d60a
Tloml-Starry <zq3620060826@163.com>
2024-09-21 13:37:14 +0800

    update: 优化

--- apps/[Sky]图鉴.js
+++ apps/[Sky]图鉴.js
@@ -1,6 +1,6 @@
 const SEARCH_PATTERNS = [
-  /^(#|\/)?((季节|好友树|复刻|晨岛|云野|雨林|峡谷|霞谷|暮土|禁阁|AURORA|表演|风行|感恩|归(巢|属)|九色鹿|梦想|魔法|破晓|潜海|圣岛|拾光|小王子|夜行|音韵|预言|重组|追(光|忆)|欧若拉|集结|凌冬|筑巢|二重奏|姆明)(季)?兑换图)$/i,
-  /^(#|\/)?(国服复刻|全图鉴参考|身高((透明)?图|进阶知识)|蜡烛合成机制|(身高)?测量规则|(20|21|22|23|24)年复刻记录)$/
+  /^(#|\/)?((?:季节|好友树|复刻|晨岛|云野|雨林|峡谷|霞谷|暮土|禁阁|AURORA|表演|风行|感恩|归(?:巢|属)|九色鹿|梦想|魔法|破晓|潜海|圣岛|拾光|小王子|夜行|音韵|预言|重组|追(?:光|忆)|欧若拉|集结|凌冬|筑巢|二重奏|姆明)(?:季)?兑换图)$/i,
+  /^(#|\/)?(国服复刻|全图鉴参考|身高(?:(?:透明)?图|进阶知识)|蜡烛合成机制|(?:身高)?测量规则)$/
 ]
 
 export class SKY extends plugin {
@@ -13,12 +13,11 @@ export class SKY extends plugin {
       rule: [
         { reg: SEARCH_PATTERNS[0], fnc: 'handleImageQuery' },
         { reg: SEARCH_PATTERNS[1], fnc: 'handleImageQuery' },
+        { reg: /^(20|21|22|23|24)年复刻记录$/, fnc: 'handleDuplicateRecording' }
       ]
     })
-  }
 
-  async handleImageQuery(e) {
-    const CATEGORIES = {
+    this.CATEGORIES = {
       LOCATIONS: ['晨岛', '云野', '雨林', '霞谷', '暮土', '禁阁'],
       SEASONS: [
         '感恩', '追光', '归属', '音韵', '魔法', '圣岛', '预言',
@@ -27,29 +26,49 @@ export class SKY extends plugin {
         '二重奏', '姆明'
       ],
       OTHER: ['好友树', '全图鉴参考', '身高图', '身高透明图', '身高进阶知识', '蜡烛合成机制', '身高测量规则', '测量规则'],
-      YEARS: ['20', '21', '22', '23', '24'],
       SPECIAL: ['复刻', '国服复刻', '节']
     }
+  }
 
-    let queryText = e.msg.replace(/#|\/|兑换图|季/g, '')
+  async handleDuplicateRecording(e) {
+    const years = ['20', '21', '22', '23', '24']
+    const images = years.map(year => segment.image(`${SKY_IMAGE_URL['A']}复刻记录/${year}年复刻记录.jpg`))
+    return e.reply(images)
+  }
 
-    let imageType = ''
-    if (CATEGORIES.LOCATIONS.includes(queryText)) {
-      imageType = '常驻兑换图'
-    } else if (CATEGORIES.SEASONS.includes(queryText) || /aurora|重组|凌冬/i.test(queryText)) {
-      imageType = '季节兑换图'
-      queryText = this.normalizeSeasonName(queryText)
-    } else if (CATEGORIES.SPECIAL.includes(queryText)) {
-      imageType = '当前'
-      queryText = queryText === '节' ? '当前季节兑换图' : '当前复刻'
-    } else if (CATEGORIES.OTHER.includes(queryText)) {
-      imageType = '其他'
-      queryText = this.normalizeOtherQuery(queryText)
-    } else if (CATEGORIES.YEARS.includes(queryText.replace(/年复刻记录/g, ''))) {
-      imageType = '复刻记录'
+  async handleImageQuery(e) {
+    const queryText = e.msg.replace(/#|\/|兑换图|季/g, '')
+    const imageInfo = this.getImageInfo(queryText)
+
+    if (!imageInfo) {
+      return e.reply('未找到相关图鉴')
     }
 
-    return e.reply([segment.image(`${SKY_IMAGE_URL['A']}${imageType}/${queryText}.${queryText === '身高透明图' ? 'png' : 'jpg'}`)])
+    const { type, name, extension } = imageInfo
+    return e.reply([segment.image(`${SKY_IMAGE_URL['A']}${type}/${name}.${extension}`)])
+  }
+
+  getImageInfo(queryText) {
+    if (this.CATEGORIES.LOCATIONS.includes(queryText)) {
+      return { type: '常驻兑换图', name: queryText, extension: 'jpg' }
+    }
+
+    if (this.CATEGORIES.SEASONS.includes(queryText) || /aurora|重组|凌冬/i.test(queryText)) {
+      return { type: '季节兑换图', name: this.normalizeSeasonName(queryText), extension: 'jpg' }
+    }
+
+    if (this.CATEGORIES.SPECIAL.includes(queryText)) {
+      const name = queryText === '节' ? '当前季节兑换图' : '当前复刻'
+      return { type: '当前', name, extension: 'jpg' }
+    }
+
+    if (this.CATEGORIES.OTHER.includes(queryText)) {
+      const name = this.normalizeOtherQuery(queryText)
+      const extension = name === '身高透明图' ? 'png' : 'jpg'
+      return { type: '其他', name, extension }
+    }
+
+    return null
   }
 
   normalizeSeasonName(name) {
