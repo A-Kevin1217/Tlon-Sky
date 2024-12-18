8c76f400caaf3322c99c0d7c25442f210232dc91
Tloml-Starry <zq3620060826@163.com>
2024-12-18 10:11:15 +0800

    增强SKY插件功能，新增按年份和月份查询碎石的功能，优化HTML结构与JavaScript逻辑，提升用户体验与可读性。

--- apps/[Sky]攻略.js
+++ apps/[Sky]攻略.js
@@ -14,7 +14,8 @@ export class SKY extends plugin {
                 { reg: /^(#|\/)?(每日|今日)?代币(位置)?$/, fnc: 'F2' },
                 { reg: /^(#|\/)?季节任务$/, fnc: 'F3' },
                 { reg: /^(#|\/)?任务图$/, fnc: 'F4' },
-                { reg: /^(#|\/)?(碎石查询|今日(红|黑|碎)石|本月(碎|红|黑)石)$/, fnc: 'F6' }
+                { reg: /^(#|\/)?(碎石查询|今日(红|黑|碎)石|本月(碎|红|黑)石)$/, fnc: 'F6' },
+                { reg: /^(#|\/)?(查询)?(\d{4})年(\d{1,2})月碎石$/, fnc: 'F7' }
             ],
         });
     }
@@ -44,5 +45,16 @@ export class SKY extends plugin {
     async F6(e) {
         await e.reply('稍等，正在截图', false, { recallMsg: 20 });
         await render('admin/光遇碎石日历', {}, { e, scale: 1.4 });
+    }
+
+    async F7(e) {
+        const match = e.msg.match(/(\d{4})年(\d{1,2})月/);
+        if (match) {
+            const year = parseInt(match[1], 10);
+            const month = parseInt(match[2], 10) - 1; // JavaScript月份从0开始
+            await render('admin/光遇碎石日历', { year, month }, { e, scale: 1.4 });
+        } else {
+            e.reply('请输入正确的年份和月份格式，例如：2023年5月');
+        }
     }
 }
