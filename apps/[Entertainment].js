243ca77b8d89be625fd4963c56866708f309877f
Tloml-Starry <zq3620060826@163.com>
2025-10-02 17:27:34 +0800

    refactor: 移除复刻预测功能及相关文档
    - 删除了娱乐插件中的复刻预测功能及其对应的HTML描述，以简化代码和文档结构

--- apps/[Entertainment].js
+++ apps/[Entertainment].js
@@ -22,34 +22,11 @@ export class EntertainmentPlugin extends plugin {
             event: 'message',
             priority: 1,
             rule: [
-                { reg: /^[#\/]?复刻预测$/, fnc: 'predictRecreation' },
                 { reg: /^[#\/]?(光遇)?(绘[画图]分享)$/, fnc: 'shareDrawing' },
                 { reg: blindBoxRegexPattern, fnc: 'storeBlindBox' },
                 { reg: /^[#\/]?随机好友(盲盒)?$/, fnc: 'getRandomFriend' }
             ]
         });
-    }
-
-    async predictRecreation(e) {
-        const namePool = [
-            '火先知', '水先知', '土先知', '风先知',
-            '刁蛮浪者', '挑衅艺伎', '敬礼护卫', '舒展大师', '跳跃舞者', '拳礼武僧',
-            '蟹语者', '击掌光农', '随性拓荒者', '肩背追光', '静光学者', '螺旋舞冠',
-            '不舍家长', '彩纸表亲', '智慧长者', '蓬头青年', '火花家长', '踏舞孩童',
-            '沉思编导', '致敬钢琴家', '抛球杂耍', '迎宾侍者', '旋转舞者', '献情演员',
-            '冷漠术士', '蟹舞者', '瞌睡木匠', '情绪草药师', '赞许壁画家', '稻草人农夫',
-            '放松日浴者', '鸣谢收藏家', '母语者', '固执登山者', '内秀书虫', '热血运动员',
-            '偷窥邮差', '熊抱雪人', '回旋大师', '跳舞艺人',
-            '正步冒险家', '傻笑童子军', '白日梦森林人', '管事小队长', '茫然植物学生', '胆小鬼学员',
-            '呼风唤雨的统治者', '沾沾自喜的自恋狂', '萎靡不振的士兵', '星光收藏家', '被逼无奈的掌灯人', '打喷嚏的地理学家',
-            '风行领航员', '风铃修补匠', '光之低语者', '天才建筑师',
-            '退休将领', '嬉笑炮手', '焦虑渔夫', '笨拙水手',
-            '老练音乐家', '健忘叙事者', '谦虚舞者', '忙碌舞台管理员'
-        ];
-
-        const [firstRandomName, secondRandomName] = lodash.sampleSize(namePool, 2);
-
-        e.reply([`以下是我猜的复刻\r[${firstRandomName}] | [${secondRandomName}]\r功能仅供娱乐`]);
     }
 
     async shareDrawing(e) {
